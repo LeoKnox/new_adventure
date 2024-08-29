68750eaecd55d26f5c5cb4644feb331b76748c1f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-29 16:58:15 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -1,6 +1,13 @@
-import allRooms from "./dungeData.js";
+import { allRooms } from "./dungeData.js";
 
 export default Build = () => {
   console.log(allRooms);
-  return <p>build a dungeon</p>;
+  return (
+    <>
+      <p>build a dungeon</p>
+      {allRooms.map((x) => {
+        <p>a{x.name}</p>;
+      })}
+    </>
+  );
 };
