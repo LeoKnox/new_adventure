f3a2b906e884ef9f1e1c7e5b3b27880b0be3a9d5
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-30 13:40:28 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -5,7 +5,7 @@ export default Build = () => {
   return (
     <>
       <p>build a dungeon</p>
-      {allRooms.map((x) => {
+      {allRooms().map((x) => {
         <p>a {x.name}</p>;
       })}
     </>
