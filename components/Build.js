ddeea97c1463c7effb9ae48b4d817491680a695c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-31 15:07:10 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { allRooms, addRoom, changeRoom } from "./dungeonData.js";
 
 export default Build = () => {
   const [rooms, setRooms] = useState(allRooms());
+  useEffect(() => {
+    setRooms(allRooms);
+  }, []);
   console.log(rooms);
   return (
     <>
       <p>build a dungeon</p>
-      <button onClick={() => setRooms(changeRoom)}>click</button>
+      <button onClick={() => setRooms(addRoom)}>click</button>
       {rooms.map((room) => (
         <tr>
           <td>{room.name}</td>
