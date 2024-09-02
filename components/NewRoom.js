a1f5eca43009988a2918d05aff36bbd1bdb1701c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-02 15:04:13 -0700

    Update NewRoom.js

--- components/NewRoom.js
+++ components/NewRoom.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { addRoom } from "./dungeonData.js";
 
 export default NewRoom = () => {
   const [name, setName] = useState("");
+  const submit = () => {
+    console.log(name);
+  };
   return (
     <>
       <td>
@@ -13,6 +17,7 @@ export default NewRoom = () => {
           onChange={(e) => setName(e.target.value)}
         />
       </td>
+      <button onClick={submit}>submit</button>
     </>
   );
 };
