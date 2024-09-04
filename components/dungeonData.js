d76339cc4c821ffcf41b44e75ba4e4c39afe8d3f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-04 12:34:57 -0700

    Update dungeonData.js

--- components/dungeonData.js
+++ components/dungeonData.js
@@ -7,14 +7,14 @@ export const allRooms = () => {
 export const addRoom = (newRoom = "ao") => {
   dungeonData = [
     ...dungeonData,
-    { id: 2, name: { newRoom }, width: 5, height: 5, x: 5, y: 5 },
+    { id: 2, name: newRoom, width: 5, height: 5, x: 5, y: 5 },
   ];
-  console.log(JSON.stringify(dungeonData));
+  //console.log(JSON.stringify(dungeonData));
   return dungeonData;
 };
 
 export const changeRoom = (newData = "chiiro") => {
   dungeonData = [{ id: 1, name: "aka", width: 5, height: 5, x: 5, y: 5 }];
-  console.log(JSON.stringify(dungeonData));
+  //console.log(JSON.stringify(dungeonData));
   return dungeonData;
 };
