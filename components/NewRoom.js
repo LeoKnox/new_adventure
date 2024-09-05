f9ac3ebc0570c16840d245564b570a8987e85576
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-05 12:43:47 -0700

    Update NewRoom.js

--- components/NewRoom.js
+++ components/NewRoom.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 
 export default NewRoom = ({ submitRoom }) => {
   const [name, setName] = useState("");
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
   const submit = () => {
     submitRoom(name);
     setName("");
@@ -16,6 +20,42 @@ export default NewRoom = ({ submitRoom }) => {
           name="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
+        />
+      </td>
+      <td>
+        Width:
+        <input
+          type="number"
+          name="width"
+          value={width}
+          onChange={(e) => setWidth(e.target.value)}
+        />
+      </td>
+      <td>
+        Height:
+        <input
+          type="number"
+          name="height"
+          value={x}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+      </td>
+      <td>
+        X:
+        <input
+          type="number"
+          name="x"
+          value={y}
+          onChange={(e) => setX(e.target.value)}
+        />
+      </td>
+      <td>
+        Y:
+        <input
+          type="number"
+          name="y"
+          value={height}
+          onChange={(e) => setY(e.target.value)}
         />
       </td>
       <button onClick={submit}>submit</button>
