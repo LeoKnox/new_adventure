cd6a79bbd9391ece76ec45455b66468c8893fb92
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-05 13:06:35 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -5,35 +5,36 @@ import NewRoom from "./NewRoom.js";
 export default Build = () => {
   const [rooms, setRooms] = useState(allRooms());
   console.log(rooms);
-  const submitRoom = (name) => {
-    let x = addRoom(name);
-    console.log(JSON.stringify(x));
-    setRooms(x);
+  const submitRoom = (name, width, height, x, y) => {
+    let temp = addRoom(name, width, height, x, y);
+    console.log(JSON.stringify(temp));
+    setRooms(temp);
   };
   return (
     <>
       <p>build a dungeon</p>
       <table>
-      <tr>
-        <th>name</th>
-        <th>width</th>
-        <th>height</th>
-        <th>x</th>
-        <th>y</th>
-        </tr>
-      {rooms.map((room) => (
         <tr>
-          <td>{room.name}</td>
-          <td>{room.width}</td>
-          <td>{room.height}</td>
-          <td>{room.x}</td>
-          <td>{room.y}</td>
+          <th>name</th>
+          <th>width</th>
+          <th>height</th>
+          <th>x</th>
+          <th>y</th>
         </tr>
-      ))}
+        {rooms.map((room) => (
+          <tr>
+            <td>{room.name}</td>
+            <td>{room.width}</td>
+            <td>{room.height}</td>
+            <td>{room.x}</td>
+            <td>{room.y}</td>
+          </tr>
+        ))}
+
+        <tr>
+          <NewRoom submitRoom={submitRoom} />
+        </tr>
       </table>
-      <tr>
-        <NewRoom submitRoom={submitRoom} />
-      </tr>
     </>
   );
 };
