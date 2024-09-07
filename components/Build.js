e9f2fca73eecbdbb294a7d98d16d328125ddd2f1
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-07 14:27:24 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -7,18 +7,16 @@ import EditRoom from "./EditRoom.js";
 export default Build = () => {
   const [isEdit, setIsEdit] = useState(true);
   const [rooms, setRooms] = useState(allRooms());
-  const loadEdit = () => {
-    return <EditRoom />
-  }
+  const loadEdit = () => {};
   const submitRoom = (name, width, height, x, y) => {
     let temp = addRoom(name, width, height, x, y);
     setRooms(temp);
   };
   return (
     <>
-      <p>build a dungeon</p>
+      <p onClick={() => setIsEdit(!isEdit)}>build a dungeon</p>
       {isEdit ? (
-        loadEdit()
+        <EditRoom />
       ) : (
         <>
           <AllRooms rooms={rooms} />
