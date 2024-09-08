f715b776afc6edebf29c0e93107e90889bfa973c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-08 14:32:46 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { singleRoom } from "./dungeonData.js";
 
 export default EditRoom = ({ newId = 1 }) => {
+  const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
+  console.log(roomEdit);
   return (
     <>
       <button>{newId}</button>
-      <p>{singleRoom(newId).name}</p>
+      <p>{roomEdit.name}</p>
     </>
   );
 };
