725d04b06198e9e11002af031669db86055b5f2f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-09 14:46:47 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -1,53 +1,56 @@
 import { useState } from "react";
-import { singleRoom } from "./dungeonData.js";
+import { singleRoom, changeRoom } from "./dungeonData.js";
 
 export default EditRoom = ({ newId = 1 }) => {
   const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
-  const hold = singleRoom(newId);
-  const [roomName, setRoomName] = useState(hold.name);
-  const changeRoom = (e) => {
-    setRoomEdit({ ...roomEdit, name: e.target.value });
+  const submitRoom = () => {
+    changeRoom(roomEdit);
   };
-  console.log(roomEdit);
   return (
     <>
       <button>{newId}</button>
       <p>{singleRoom(newId).name}</p>
-      <p>Name:
-      <input
-        type="text"
-        value={roomEdit.name}
-        onChange={(e) => setRoomEdit({ ...roomEdit, name: e.target.value })}
-      />
+      <p>
+        Name:
+        <input
+          type="text"
+          value={roomEdit.name}
+          onChange={(e) => setRoomEdit({ ...roomEdit, name: e.target.value })}
+        />
       </p>
-      <p>Width:
-      <input
-        type="number"
-        value={roomEdit.width}
-        onChange={(e) => setRoomEdit({ ...roomEdit, width: e.target.value })}
-      />
+      <p>
+        Width:
+        <input
+          type="number"
+          value={roomEdit.width}
+          onChange={(e) => setRoomEdit({ ...roomEdit, width: e.target.value })}
+        />
       </p>
-      <p>Height:
-      <input
-        type="number"
-        value={roomEdit.height}
-        onChange={(e) => setRoomEdit({ ...roomEdit, height: e.target.value })}
-      />
+      <p>
+        Height:
+        <input
+          type="number"
+          value={roomEdit.height}
+          onChange={(e) => setRoomEdit({ ...roomEdit, height: e.target.value })}
+        />
       </p>
-      <p>X:
-      <input
-        type="number"
-        value={roomEdit.x}
-        onChange={(e) => setRoomEdit({ ...roomEdit, x: e.target.value })}
-      />
+      <p>
+        X:
+        <input
+          type="number"
+          value={roomEdit.x}
+          onChange={(e) => setRoomEdit({ ...roomEdit, x: e.target.value })}
+        />
       </p>
-      <p>Y:
-      <input
-        type="number"
-        value={roomEdit.y}
-        onChange={(e) => setRoomEdit({ ...roomEdit, y: e.target.value })}
-      />
+      <p>
+        Y:
+        <input
+          type="number"
+          value={roomEdit.y}
+          onChange={(e) => setRoomEdit({ ...roomEdit, y: e.target.value })}
+        />
       </p>
+      <button onClick={submitRoom}>Submit</button>
     </>
   );
 };
