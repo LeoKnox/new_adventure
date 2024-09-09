ddfd56016c5731927e65a2d7a61183bdcd60c62d
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-09 14:13:49 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -13,10 +13,31 @@ export default EditRoom = ({ newId = 1 }) => {
     <>
       <button>{newId}</button>
       <p>{singleRoom(newId).name}</p>
+      <label>Name:</label>
       <input
         type="text"
         value={roomEdit.name}
         onChange={(e) => setRoomEdit({ ...roomEdit, name: e.target.value })}
+      />
+      <input
+        type="number"
+        value={roomEdit.width}
+        onChange={(e) => setRoomEdit({ ...roomEdit, width: e.target.value })}
+      />
+      <input
+        type="number"
+        value={roomEdit.height}
+        onChange={(e) => setRoomEdit({ ...roomEdit, height: e.target.value })}
+      />
+      <input
+        type="number"
+        value={roomEdit.x}
+        onChange={(e) => setRoomEdit({ ...roomEdit, x: e.target.value })}
+      />
+      <input
+        type="number"
+        value={roomEdit.y}
+        onChange={(e) => setRoomEdit({ ...roomEdit, y: e.target.value })}
       />
     </>
   );
