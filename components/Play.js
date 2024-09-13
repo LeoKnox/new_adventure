d58694c7fc4d3d366ad2b0f6374f36b1a1bc09de
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-13 16:56:32 -0700

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -1,3 +1,12 @@
+import allCharacters from "./characterData.js";
+
 export default Play = () => {
-  return <h2>Play game</h2>;
+  return (
+    <>
+      <h2>Play game</h2>
+      {allCharacters.map((character) => {
+        <p>{character.name}</p>;
+      })}
+    </>
+  );
 };
