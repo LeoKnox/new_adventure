4d886081a8109fa3904608407b898c6ca8c4a7e6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-13 16:52:58 -0700

    Create chatacterData.js

---
+++ components/chatacterData.js
@@ -0,0 +1,8 @@
+let characterData = [
+  { id: 1, name: "ao", atk: 10, def: 10, weapon: ["spear"] },
+  { id: 2, name: "aka", atk: 12, def: 9, weapon: ["sword"] },
+];
+
+export const allCharacters = () => {
+  return characterData;
+};
