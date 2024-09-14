544dd821437720d307391fe378baae86d83aba29
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-14 14:19:36 -0700

    Update View.js

--- components/View.js
+++ components/View.js
@@ -1,3 +1,10 @@
+import { singleCharacter } from "./characterData.js";
+
 export default View = ({ characterId }) => {
-  return <h3>Character {characterId}</h3>;
+  console.log(JSON.stringify({ singleCharacter }));
+  return (
+    <>
+      <h3>Character {characterId}</h3>
+    </>
+  );
 };
