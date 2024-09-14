17574da21adc535df115aa24c57bfb43d3220997
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-13 17:00:16 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -3,7 +3,7 @@ import Build from "./Build.js";
 import Play from "./Play.js";
 
 export default Home = () => {
-  const [play, setPlay] = useState(false);
+  const [play, setPlay] = useState(true);
   return (
     <div>
       <h1>Home Page</h1>
