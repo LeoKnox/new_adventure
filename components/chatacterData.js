33b7e9e49d41d0300c758cbdd0f30fed96fd8bdf
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-14 12:03:07 -0700

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -1,6 +1,6 @@
 let characterData = [
-  { id: 1, name: "ao", atk: 10, def: 10, weapon: ["spear"] },
-  { id: 2, name: "aka", atk: 12, def: 9, weapon: ["sword"] },
+  { id: 1, name: "ao", lvl: 1, atk: 10, def: 10, weapon: ["spear"] },
+  { id: 2, name: "aka", lvl: 1, atk: 12, def: 9, weapon: ["sword"] },
 ];
 
 export const allCharacters = () => {
