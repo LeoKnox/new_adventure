8e81e1661bb6165dbae9b0076589a493d095c57f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-16 11:55:17 -0700

    Update NewCharacter.js

--- components/NewCharacter.js
+++ components/NewCharacter.js
@@ -1,12 +1,16 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default NewCharacter = () => {
-    const [name, setName] = useState("new");
+  const [name, setName] = useState("");
   return (
     <>
       <tr>
-      <td>
-        <input type="text" value={name} />
+        <td>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </td>
         <td>
           <button>AddCharacter</button>
