444b0bbded51dc7eb87f602ed1483259ac07b454
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-19 14:59:53 -0700

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -9,55 +9,9 @@ import NewCharacter from "./NewCharacter.js";
 import View from "./View.js";
 
 export default Play = () => {
-  const [characters, setCharacters] = useState(allCharacters());
-  const [characterId, setCharacterId] = useState(0);
-  const showCharacter = (id) => {
-    setCharacterId(id);
-  };
-  const submitCharacter = () => {
-    setCharacters(allCharacters());
-  };
-  const removeCharacter = (deleteId) => {
-    deleteCharacter(deleteId);
-    setCharacters(allCharacters());
-  };
   return (
     <>
       <h2>Play game</h2>
-      {characterId == 0 ? (
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>lvl</th>
-            <th>atk</th>
-            <th>def</th>
-            <th>actions</th>
-          </tr>
-          {characters.map((character) => (
-            <tr>
-              <td>{character.name}</td>
-              <td>{character.lvl}</td>
-              <td>{character.atk}</td>
-              <td>{character.def}</td>
-              <td>
-                <button onClick={() => showCharacter(character.id)}>
-                  View
-                </button>
-                <button onClick={() => removeCharacter(character.id)}>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-
-          <NewCharacter submitCharacter={submitCharacter} />
-        </table>
-      ) : (
-        <>
-          <View characterId={characterId} />
-          <button onClick={() => setCharacterId(0)}>Back</button>
-        </>
-      )}
     </>
   );
 };
