4f91423bcbb7d7f075aff3ef6d2ee7703c08c744
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-19 14:55:14 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -9,11 +9,11 @@ export default Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      {page}
-      <button onClick={() => setPlay(true)}>Play</button>
-      <button onClick={() => setPlay(false)}>Build</button>
+
+      <button onClick={() => setPage(<Play />)}>Play</button>
+      <button onClick={() => setPage(<Build />)}>Build</button>
       <button onClick={() => setPage(<Build />)}>Change</button>
-      {play ? <Characters /> : <Build />}
+      {page}
     </div>
   );
 };
