5fd2ed67406db04e9995f834a55bba034f4427ee
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-19 14:52:26 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import Build from "./Build.js";
+import Characters from "./Characters.js";
 import Play from "./Play.js";
 
 export default Home = () => {
   const [play, setPlay] = useState(true);
+  const [page, setPage] = useState(<Characters />);
   return (
     <div>
       <h1>Home Page</h1>
+      {page}
       <button onClick={() => setPlay(true)}>Play</button>
       <button onClick={() => setPlay(false)}>Build</button>
-      {play ? <Play /> : <Build />}
+
+      {play ? <Characters /> : <Build />}
     </div>
   );
 };
