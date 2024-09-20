f609eddf004127a739d327ccf4e818ba0c13c2c0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-20 15:24:55 -0700

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import {allCharacters} from "./characterData.js";
+import { allCharacters } from "./characterData.js";
+import SelectCharacter from "./SelectCharacter.js";
 import View from "./View.js";
 
 export default Play = () => {
-    const [playerId, setPlayerId] = useState(1);
+  const [playerId, setPlayerId] = useState(1);
   return (
     <>
       <h2>Play game</h2>
       {playerId}
+      <SelectCharacter />
     </>
   );
 };
