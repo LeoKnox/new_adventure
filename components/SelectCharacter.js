d4dd0e36e9c83c826e856f888267b9ae55a22450
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-20 18:40:23 -0700

    Update SelectCharacter.js

--- components/SelectCharacter.js
+++ components/SelectCharacter.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { allCharacters } from "./characterData.js";
 
-export default SelectCharacter = () => {
+export default SelectCharacter = ({setPlayerId}) => {
   const [characterList, setCharacterList] = useState(allCharacters());
   return (
     <>
       <p>select character</p>
-      {characterList.map((character) => (<p>{character.name}</p>) )}
+      {characterList.map((character) => (
+        <p>{character.name}</p>
+      ))}
     </>
   );
 };
