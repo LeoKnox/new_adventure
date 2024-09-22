30a53bcdce2a4d326561721e04e6d9197c985275
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-22 13:53:58 -0700

    Update SelectCharacter.js

--- components/SelectCharacter.js
+++ components/SelectCharacter.js
@@ -3,11 +3,9 @@ import { allCharacters } from "./characterData.js";
 
 export default SelectCharacter = ({ setPlayerId }) => {
   const [characterList, setCharacterList] = useState(allCharacters());
-  const [cr, setCharacter] = useState(0);
   return (
     <>
       <p>select character</p>
-      <p>{cr}</p>
       <select onChange={(e) => setPlayerId(e.target.value)}>
         <option disabled selected>
           Select Character
