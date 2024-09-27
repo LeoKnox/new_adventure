1f5db4d509d109e8e7f2a7073985a27dbbc956e3
LeoKnox <LeoKnox@users.noreply.github.com>
2024-09-27 15:51:15 -0700

    Update AddWeapon.js

--- components/AddWeapon.js
+++ components/AddWeapon.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { addWeapon } from "./characterData.js";
 
 export default AddWeapon = ({
@@ -5,11 +6,18 @@ export default AddWeapon = ({
   updateWeapon,
   characterWeapons,
 }) => {
+  const [listWeapon, setListWeapon] = useState("");
   return (
     <>
       {characterWeapons}
+      <br />
+      <select>
+        <option>kama</option>
+        <option>sai</option>
+      </select>
+      <br />
       <button onClick={() => characterWeapons.push("migi")}>right</button>
-      <button onClick={() => updateWeapon(characterId)}>Add Weapon</button>
+      <button onClick={() => updateWeapon()}>Add Weapon</button>
     </>
   );
 };
