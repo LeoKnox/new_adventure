0c3004daef3abaed0dc3e90a8c5032b4cb6f34d0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-06 16:00:06 -0700

    Update AddWeapon.js

--- components/AddWeapon.js
+++ components/AddWeapon.js
@@ -2,14 +2,12 @@ import { useState } from "react";
 
 export default AddWeapon = ({ updateWeapon, characterWeapons }) => {
   const [listWeapon, setListWeapon] = useState("kama");
-  const [charWeapons, setCharWeapons] = useState(characterWeapons);
   // will set state to first on weapon list
   const addNewWeapon = () => {
     updateWeapon(listWeapon);
   };
   return (
     <div>
-      {charWeapons}
       <p>Add {listWeapon}</p>
       <br />
       <select onChange={(e) => setListWeapon(e.target.value)}>
