73998e77849727278dbf20a19abeff42324459bf
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-07 12:25:35 -0700

    Update DisplayWeapon.js

--- components/DisplayWeapon.js
+++ components/DisplayWeapon.js
@@ -1,9 +1,13 @@
-export default DisplayWeapon = ({ characterWeapon }) => {
+export default DisplayWeapon = ([characterWeapon]) => {
   console.log(characterWeapon);
   return (
     <div>
       <ul>
-        {(characterWeapon.map = (weapon, i) => <li key={i}>x{weapon}</li>)}
+        {
+          (characterWeapon.map = (weapon, i) => {
+            <li key={i}>x{weapon}</li>;
+          })
+        }
       </ul>
     </div>
   );
