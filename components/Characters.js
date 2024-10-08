720a2f2ec6b041d53f2548974936213866120da0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-08 12:25:14 -0700

    Update Characters.js

--- components/Characters.js
+++ components/Characters.js
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import {
-  addCharacter,
   allCharacters,
   deleteCharacter,
 } from "./characterData.js";
-import Delete from "./Delete.js";
 import NewCharacter from "./NewCharacter.js";
 import View from "./View.js";
 
