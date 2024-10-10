383e9f59c370f083a6907dddd82bbe8080748803
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-10 14:44:06 -0700

    Update DisplayArmor.js

--- components/DisplayArmor.js
+++ components/DisplayArmor.js
@@ -1,42 +1,7 @@
-import { useState, useEffect } from "react";
-import { singleCharacter, addWeapon, addInventory } from "./characterData.js";
-import AddArmor from "./AddArmor.js";
-import AddWeapon from "./AddWeapon.js";
-import DisplayArmor from "./DisplayArmor.js";
-import DisplayWeapon from "./DisplayWeapon.js";
-
-export default View = ({ characterId }) => {
-  const [character, setCharacter] = useState({});
-  const updateWeapon = (newWeapon = "bat") => {
-    addWeapon(characterId, newWeapon);
-    setCharacter(singleCharacter(characterId));
-  };
-  const updateInventory = (type = "armor", newItem = "leather") => {
-    console.log("inventory");
-    addInventory();
-  };
-  useEffect(() => {
-    let temp = {};
-    temp = singleCharacter(characterId);
-    setCharacter(temp);
-  }, []);
-
+export default DisplayArmor = () => {
   return (
     <>
-      <h3>Character</h3>
-      <p>Name: {character.name}</p>
-      <p>{character.weapon}</p>
-      <p>Lvl: {character.lvl}</p>
-      <p>Atk: {character.atk}</p>
-      <p>Def: {character.def}</p>
-      <AddWeapon
-        characterId={characterId}
-        updateWeapon={updateWeapon}
-        characterWeapons={character.weapon}
-      />
-      <DisplayWeapon characterWeapon={character.weapon} />
-      <AddArmor updateInventory={updateInventory} />
-      <DisplayArmor />
+      <h3>list of armor</h3>
     </>
   );
 };
