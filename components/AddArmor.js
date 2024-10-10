de26f8a12c731822fca70575ca4d8a4f35dc49ae
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-10 13:47:13 -0700

    Update AddArmor.js

--- components/AddArmor.js
+++ components/AddArmor.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-export default AddArmor = () => {
+export default AddArmor = ({ updateInventory }) => {
   const [newArmor, setNewArmor] = useState("Leather");
   const addArmor = () => {
-    console.log("AA armor");
+    updateInventory();
   };
   return (
     <>
