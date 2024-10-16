afe450da3598ffab0c48738362b00f2865e125ba
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-16 16:44:27 -0700

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -1,3 +1,5 @@
-let playData = [
-    {charId: 1, x: 1, y:1}
-]
+let playData = [{ charId: 1, x: 1, y: 1 }];
+
+export const charLocation = (id=1) => {
+    return {charX: playData[0].x, charY: playData[0].y}
+}
