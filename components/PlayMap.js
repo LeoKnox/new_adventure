954440eb17c92b2c0b49cc0c295a16535f08a573
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-18 15:31:21 -0700

    Update PlayMap.js

--- components/PlayMap.js
+++ components/PlayMap.js
@@ -1,20 +1,14 @@
 import { floorSVG, wallSVG } from "./svgData";
 
 export default PlayMap = () => {
-    let x = 40;
+  let x = 40;
   return (
     <>
       <p>Map</p>
-      <svg
-        fill="#000000"
-        version="1.1"
-        id="Capa_1"
-        width=x+"px"
-        height="80px"
-        viewBox="0 0 535.51 535.51"
-      >
-        {floorSVG()}
-      </svg>
+      <tr>
+        <td style={{width:60}}>{floorSVG()}</td>
+        <td>{wallSVG()}</td>
+      </tr>
     </>
   );
 };
