369bda88f26b9b69a3b2e40bd806c9d0f421da48
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-19 14:43:54 -0700

    Create DrawMap.js

---
+++ components/DrawMap.js
@@ -0,0 +1,33 @@
+import { floorSVG, wallSVG } from "./svgData";
+
+export default DrawMap = (x = 10) => {
+  return (
+    <>
+      <tr>
+        {[...Array(x + 2)].map((z, i) => (
+          <>
+            <td>{wallSVG()}</td>
+          </>
+        ))}
+      </tr>
+      {[...Array(x)].map((y, i) => (
+        <tr>
+          <td> {wallSVG()}</td>
+          {[...Array(x)].map((z, i) => (
+            <>
+              <td>{floorSVG()}</td>
+            </>
+          ))}
+          <td> {wallSVG()}</td>
+        </tr>
+      ))}
+      <tr>
+        {[...Array(x + 2)].map((z, i) => (
+          <>
+            <td>{wallSVG()}</td>
+          </>
+        ))}
+      </tr>
+    </>
+  );
+};
