74429f43182e274d825731783a3b8a9316112b6b
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-22 14:45:30 -0700

    Update PlayMap.js

--- components/PlayMap.js
+++ components/PlayMap.js
@@ -3,10 +3,14 @@ import DrawMap from "./DrawMap.js";
 
 export default PlayMap = () => {
   let width = 10;
-  let height = 8
+  let height = 6;
+  const addCharacter = () => {
+    // id="0:0"
+    console.log("added character");
+  };
   return (
     <>
-      <p>Map</p>
+      <button onClick={addCharacter}>character</button>
       <table>
         <DrawMap width={width} height={height} />
       </table>
