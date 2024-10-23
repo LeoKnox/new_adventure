96a4f045e3f78830e4e51713176f59c9cc7d0de4
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-23 16:36:51 -0700

    Update PlayMap.js

--- components/PlayMap.js
+++ components/PlayMap.js
@@ -6,12 +6,15 @@ import DrawMap from "./DrawMap.js";
 export default PlayMap = () => {
   let width = 10;
   let height = 6;
-  const [test, setTest] = useState([wallSVG(), floorSVG()]);
+  const [test, setTest] = useState([
+    [wallSVG(), wallSVG()],
+    [wallSVG(), wallSVG()],
+  ]);
   const addCharacter = () => {
     // id="0:0"
     //document.getElementById([0, 0]).innerHTML = <DrawCharacter />;
-    let temp = test;
-    temp[1] = wallSVG();
+    let temp = [...test];
+    temp[1][1] = floorSVG();
     setTest(temp);
     console.log(temp);
   };
