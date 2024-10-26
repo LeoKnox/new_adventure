549a69cc1364dfc931ae75afd25727b7b41a3d3a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-26 13:40:00 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -17,7 +17,7 @@ export default DrawMap = ({ width = 10, height, x = 10 }) => {
     let newtemp = [...mapState[1].props.children];
     newtemp[column] = (
       <td>
-        {floorSVG()}
+        <div style={{ opacity: "60%" }}>{floorSVG()}</div>
         <div style={{ position: "absolute" }}>{warriorSVG()}</div>
       </td>
     );
