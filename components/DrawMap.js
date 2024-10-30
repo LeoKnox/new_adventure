63af76d11162cfb080754c15e60a338df233038a
LeoKnox <LeoKnox@users.noreply.github.com>
2024-10-29 21:00:17 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -3,7 +3,7 @@ import { floorSVG, wallSVG, warriorSVG } from "./svgData";
 
 export default DrawMap = ({ width = 10, height, x = 10 }) => {
   const [mapState, setMapState] = useState(
-    Array.from(Array(5), () => Array.from(Array(7), () => <td>+</td>))
+    Array.from(Array(2), () => Array.from(Array(2), () => <td>+</td>))
   );
   useEffect(() => {
     let temp = [];
