25b0999fd040127696600209e3d0259240cd05e6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-11-03 20:12:07 -0800

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -5,6 +5,11 @@ export default DrawMap = ({ width = 10, height, x = 10 }) => {
   const [mapState, setMapState] = useState(() => {
     let temp = [];
     let tempRow = [];
+    for (let x = 0; x < 10; x++) {
+      tempRow.push("*")
+    }
+    temp.push(tempRow);
+    tempRow=[];
     for (let x = 0; x < 6; x++) {
       tempRow.push("*");
       for (let y = 0; y < 8; y++) {
@@ -25,9 +30,11 @@ export default DrawMap = ({ width = 10, height, x = 10 }) => {
   return (
     <>
       {mapState.map((row) => (
-        <tr>{row.map((tile) => (
-          <td>{tile}</td>
-        ))}</tr>
+        <tr>
+          {row.map((tile) => (
+            <td>{tile}</td>
+          ))}
+        </tr>
       ))}
       <button onClick={() => addCharacter(2, 3)}>cc</button>
       <tr>
