f86b9f4a277536df1755de07d9d7736ceb2083d8
LeoKnox <LeoKnox@users.noreply.github.com>
2024-11-07 14:15:22 -0800

    Update PlayMap.js

--- components/PlayMap.js
+++ components/PlayMap.js
@@ -1,103 +1,32 @@
-import { useState, useEffect, cloneElement } from "react";
+import { useState } from "react";
 import { floorSVG, wallSVG, warriorSVG } from "./svgData";
+import DrawCharacer from "./DrawCharacter.js";
+import DrawMap from "./DrawMap.js";
 
-export default DrawMap = ({ width = 10, height = 10, x = 2, y = 3, sety }) => {
-  const [mapState, setMapState] = useState(() => {
-    let temp = [];
-    let tempRow = [];
-    for (let x = 0; x < 10; x++) {
-      tempRow.push(wallSVG());
-    }
-    temp.push(tempRow);
-    tempRow = [];
-    for (let x = 0; x < 6; x++) {
-      tempRow.push(wallSVG());
-      for (let y = 0; y < 8; y++) {
-        tempRow.push(floorSVG());
-      }
-      tempRow.push(wallSVG());
-      temp.push(tempRow);
-      tempRow = [];
-    }
-    for (let x = 0; x < 10; x++) {
-      tempRow.push(wallSVG());
-    }
-    temp.push(tempRow);
-    tempRow = [];
-    return temp;
-  });
-
-  const addCharacter = (x, y) => {
-    let tempRow = [...mapState];
-    let temp = [...mapState[x]];
-    temp[y] = (
-      <td>
-        <div style={{ position: "absolute", opacity: "60%" }}>{floorSVG()}</div>
-        <div style={{ position: "relative" }}>{warriorSVG()}</div>
-      </td>
-    );
-    tempRow[x] = temp;
-    setMapState(tempRow);
-  };
-  const moveCharacter = (e) => {
-    console.log(e.target.value);
-    let newy = e.target.value + 1;
-    let tempRow = [...mapState];
-    let temp = [...mapState[x]];
-    temp[y] = (
-      <td>
-        <div>{floorSVG()}</div>
-      </td>
-    );
-    tempRow[x] = temp;
-    temp[newy] = (
-      <td>
-        <div style={{ position: "absolute", opacity: "60%" }}>{floorSVG()}</div>
-        <div style={{ position: "relative" }}>{warriorSVG()}</div>
-      </td>
-    );
-    tempRow[x] = temp;
-    sety(newy);
-    setMapState(tempRow);
-    console.log("move");
-  };
-  const moveCharacterDown = () => {
-    let tempRow = [...mapState];
-    let temp = [...mapState[x]];
-    temp[y] = (
-      <td>
-        <div>{floorSVG()}</div>
-      </td>
-    );
-    tempRow[x] = temp;
-    temp[y] = (
-      <td>
-        <div style={{ position: "absolute", opacity: "60%" }}>{floorSVG()}</div>
-        <div style={{ position: "relative" }}>{warriorSVG()}</div>
-      </td>
-    );
-    tempRow[x + 1] = temp;
-    setMapState(tempRow);
-    console.log("move");
+export default PlayMap = () => {
+  let width = 10;
+  let height = 6;
+  const [test, setTest] = useState([
+    [wallSVG(), wallSVG()],
+    [wallSVG(), wallSVG()],
+  ]);
+  const [x, setx] = useState(1);
+  const [y, sety] = useState(1);
+  const addCharacter = (row = 1, column = 0) => {
+    // id="0:0"
+    //document.getElementById([0, 0]).innerHTML = <DrawCharacter />;
+    let temp = [...test];
+    let newtemp = [...test[row]];
+    newtemp[column] = floorSVG();
+    temp[row] = newtemp;
+    setTest(temp);
+    console.log(newtemp);
   };
   return (
     <>
-      {mapState.map((row) => (
-        <tr>
-          {row.map((tile) => (
-            <td>{tile}</td>
-          ))}
-        </tr>
-      ))}
-      <td>
-        <button onClick={() => addCharacter(x, y)}>cc</button>
-      </td>
-      <td>
-        <button value="right" onClick={(e) => moveCharacter(e)}>
-          right
-        </button>
-      </td>
-      <button onClick={moveCharacterDown}>down</button>
+      <table>
+        <DrawMap width={width} height={height} x={x} y={y} sety={sety} />
+      </table>
     </>
   );
 };
