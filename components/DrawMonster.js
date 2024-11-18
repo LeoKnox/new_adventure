5a624135fdb952b297add13970d3085c8c51ca08
LeoKnox <LeoKnox@users.noreply.github.com>
2024-11-17 18:11:56 -0800

    Update DrawMonster.js

--- components/DrawMonster.js
+++ components/DrawMonster.js
@@ -1,9 +1,11 @@
 import { demonSVG, wallSVG } from "./svgData.js";
 
-export default Monster = () => {
+export default Monster = (background) => {
   return (
     <td>
-        <div style={{ position: "absolute", opacity: "60%" }}>{wallSVG()}</div>
+      <div style={{ position: "absolute", opacity: "60%" }}>
+        {() => background()}
+      </div>
       <div style={{ position: "relative" }}>{demonSVG()}</div>
     </td>
   );
