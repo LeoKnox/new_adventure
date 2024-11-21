19a3b14db755b4bb7c21c35ffb4a2bc30acd4c8f
LeoKnox <LeoKnox@users.noreply.github.com>
2024-11-21 15:03:14 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -1,5 +1,12 @@
-let playData = {character:{ charId: 1, x: 1, y: 1 }, monster: [{ icon: "demon", x: 8, y: 3 }]};
+let playData = {
+  character: { charId: 1, x: 1, y: 1 },
+  monster: [{ icon: "demon", x: 8, y: 3 }],
+};
 
 export const charLocation = (id = 1) => {
   return { charX: playData.character.x, charY: playData.character.y };
 };
+
+export const updateMonster = () => {
+  playData.monster[0].x = 7;
+}
