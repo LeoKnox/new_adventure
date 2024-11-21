444442e9d58c2bbca25ca6940ebfa71a223248d3
LeoKnox <LeoKnox@users.noreply.github.com>
2024-11-20 18:23:23 -0800

    Update PlayMap.js

--- components/PlayMap.js
+++ components/PlayMap.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import { floorSVG, wallSVG, warriorSVG } from "./svgData";
+import {singleRoom} from "./dungeonData.js";
 import DrawCharacer from "./DrawCharacter.js";
 import DrawMap from "./DrawMap.js";
 
 export default PlayMap = () => {
-  let width = 10;
-  let height = 6;
+  let newMap = singleRoom(1)
+  let width = newMap.width;
+  let height = newMap.height;
   const [test, setTest] = useState([
     [wallSVG(), wallSVG()],
     [wallSVG(), wallSVG()],
   ]);
   const [x, setx] = useState(1);
   const [y, sety] = useState(1);
-  
+
   return (
     <>
       <table>
