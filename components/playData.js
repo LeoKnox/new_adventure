e56173c78872df534331f0d548d3b7438a0274ad
LeoKnox <LeoKnox@users.noreply.github.com>
2024-12-23 11:47:15 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -4,7 +4,7 @@ import DrawMonster from "./DrawMonster.js";
 let playData = {
   character: { charId: 1, x: 1, y: 1 },
   /*monster: [{ icon: "demon", x: 8, y: 3 }],*/
-  moster: {
+  monster: {
     1: [
       {
         oldx: 1,
