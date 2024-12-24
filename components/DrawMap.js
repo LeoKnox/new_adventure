3a7e9ddeeea36ed437a919e3be9aa7c9d150c237
LeoKnox <LeoKnox@users.noreply.github.com>
2024-12-24 15:58:38 -0800

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -116,9 +116,7 @@ export default DrawMap = ({
         newRow[child.oldx] = floorSVG();
         //newRow[child.newx] = child.tile;
         //newRow[child.newx] = child.tile.props.background;
-        newRow = newRow.map((ite, ind) =>
-          ind != 7 ? child.tile.props.background : ite
-        );
+        newRow = newRow.map((ite, ind) => (ind == 7 ? <td>r</td> : ite));
         console.log(newRow);
         newGrid[index] = newRow;
       });
