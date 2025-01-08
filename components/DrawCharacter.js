095700243c0612bdf0cb480b618e1fe1b3d7e986
LeoKnox <LeoKnox@users.noreply.github.com>
2025-01-08 14:04:55 -0800

    Update DrawCharacter.js

--- components/DrawCharacter.js
+++ components/DrawCharacter.js
@@ -1,5 +1,10 @@
 import { warriorSVG } from "./svgData";
 
 export default DrawCharacter = () => {
-  return <td>{warriorSVG()}</td>;
+  return (
+    <td>
+      <div style={{ position: "absolute", opacity: "60%" }}>{background}</div>
+      <div style={{ position: "relative" }}>{demonSVG()}</div>
+    </td>
+  );
 };
