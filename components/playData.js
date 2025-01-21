56fb89bd53853d349453807a1b56c3d78bdc259c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-01-20 20:23:09 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -33,11 +33,12 @@ export const charLocation = (id = 1) => {
 };
 export const changeLeft = () => {
   console.log("change left");
-  let t=playData["character"]
-  t[0].newx = t[0].oldx+1;
+  let t = playData["character"];
+  t[0].newx = t[0].oldx + 1;
   playData["character"] = t;
+  charLocation();
   console.log(playData);
-}
+};
 
 export const updateMonster = (direction = "up") => {
   let tempData = playData.monster;
