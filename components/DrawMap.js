2d8ef92e88d2db4b66497ac842b792c61e52b51c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-01-30 12:46:15 -0800

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -1,99 +1,234 @@
+import { useState, useEffect, cloneElement } from "react";
 import { floorSVG, wallSVG, warriorSVG } from "./svgData";
+import {
+  changeLeft,
+  changeRight,
+  charUpdate,
+  charLocation,
+  updateMonster,
+  singleMonster,
+  change,
+  updateCharacter,
+} from "./playData.js";
 import DrawMonster from "./DrawMonster.js";
 import DrawCharacter from "./DrawCharacter.js";
 
-let playData = {
-  character: {
-    1: [{ oldx: 3, newx: 3, tile: <DrawCharacter background={floorSVG()} /> }],
-  },
-  monster: {
-    3: [{ oldx: 8, newx: 8, tile: <DrawMonster background={floorSVG()} /> }],
-  },
-};
-let mobData = {};
-mobData = {
-  3: [{ oldx: 8, newx: 8, tile: <DrawMonster background={floorSVG()} /> }],
-};
+export default DrawMap = ({
+  width = 10,
+  height = 10,
+  x = 2,
+  y = 3,
+  sety,
+  setx,
+}) => {
+  let mobs = singleMonster();
+  const [pagetest, setpagetest] = useState(warriorSVG());
+  const [char, setChar] = useState(charLocation());
+  const [mapState, setMapState] = useState(() => {
+    let temp = [];
+    let tempRow = [];
+    for (let x = 0; x < width + 2; x++) {
+      tempRow.push(wallSVG());
+    }
+    temp.push(tempRow);
+    tempRow = [];
+    for (let x = 0; x < height; x++) {
+      tempRow.push(wallSVG());
+      for (let y = 0; y < width; y++) {
+        tempRow.push(floorSVG());
+      }
+      tempRow.push(wallSVG());
+      temp.push(tempRow);
+      tempRow = [];
+    }
+    for (let x = 0; x < width + 2; x++) {
+      tempRow.push(wallSVG());
+    }
+    temp.push(tempRow);
+    tempRow = [];
+    return temp;
+  });
 
-export const charUpdate = (
-  newChar = {
-    1: [{ oldx: 3, newx: 2, tile: <DrawCharacter background={floorSVG()} /> }],
-  }
-) => {
-  console.log("charUpdate");
-  playData.character = newChar;
-  let temp = newChar;
-  playData.character[1][0].oldx = playData.char[1][0].newx;
-  //newChar = temp;
-};
+  useEffect(() => {
+    console.log("ue keydown");
+    const onKeyDown = (e) => {
+      if (e.key === "w") {
+        //moveCharacter("up");
+      } else if (e.key === "s") {
+        //moveCharacter("down");
+      } else if (e.key === "d") {
+        let temp = {
+          1: [
+            {
+              oldx: 1,
+              newx: 2,
+              tile: (
+                <td>
+                  <div style={{ position: "absolute", opacity: "60%" }}>
+                    {floorSVG()}
+                  </div>
+                  <div style={{ position: "relative" }}>{warriorSVG()}</div>
+                </td>
+              ),
+            },
+          ],
+        };
+        modifyTable(temp);
+      } else if (e.key === "a") {
+        //moveCharacter("left");
+      }
+    };
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, []);
 
-export const updateCharacter = (newData) => {
-  console.log("update character");
-  playData["character"] = newData;
-  console.log(newData);
-};
-export const charLocation = (id = 1) => {
-  console.log("char loc");
-  return playData["character"];
-};
-export const changeDown = () => {
-  console.log("change down");
-  let t = playData["character"];
-}
-export const changeLeft = () => {
-  console.log("change left");
-  let t = playData["character"];
-  t[1][0].newx = t[1][0].oldx - 1;
-  playData["character"] = t;
-};
-export const changeRight = () => {
-  console.log("change right");
-  let t = playData["character"];
-  t[1][0].newx = t[1][0].oldx + 1;
-  playData["character"] = t;
-};
+  useEffect(() => {
+    console.log("draw character");
+    let tempRow = [...mapState];
+    let temp = [...mapState[x]];
+    temp[y] = char;
+    tempRow[x] = temp;
+    let chartemp = charLocation();
+    for (let ct in chartemp) {
+      mobs[ct] = chartemp[ct];
+    }
+    modifyTable(mobs);
+  }, []);
 
-export const updateMonster = (direction = "up") => {
-  let tempData = playData.monster;
-  if (direction === "left") {
-    tempData = {
+  const moveMob = () => {
+    console.log("move mob");
+
+    let t = singleMonster();
+
+    modifyTable(t);
+    setMapState(t);
+    //char = updateMonster(t);
+  };
+
+  const modifyTable = (
+    objMove = {
       3: [
         {
-          oldx: playData.monster[3][0].newx,
-          newx: playData.monster[3][0].newx - 1,
-          tile: <DrawMonster background={floorSVG()} />,
+          oldx: 8,
+          newx: 7,
+          tile: <DrawMonster background={wallSVG()} />,
         },
       ],
-    };
-  }
-  if (direction === "up") {
-    let a = Object.keys(playData.monster)[0];
-    console.log("up");
-    (tempData[a] = [
-      {
-        oldx: playData.monster[a][0].newx,
-        newx: playData.monster[a][0].newx,
-        tile: floorSVG(),
-      },
-    ]),
-      (tempData[a - 1] = [
-        {
-          oldx: playData.monster[a][0].newx,
-          newx: playData.monster[a][0].newx,
-          tile: <DrawMonster background={floorSVG()} />,
-        },
-      ]),
-      (mobData = tempData);
-  }
-};
-
-export const change = () => {
-  console.log("change");
-  let a = Object.keys(playData.monster)[1];
-  delete playData.monster[a];
-};
-
-export const singleMonster = () => {
-  console.log("mob data");
-  return mobData;
+    }
+  ) => {
+    console.log("modify table");
+    let newGrid = [...mapState];
+    let newRow = [];
+    for (index in objMove) {
+      newRow = [...mapState[index]];
+      objMove[parseInt(index)].map((child) => {
+        newRow[child.oldx] = floorSVG();
+        newRow[child.newx] = child.tile;
+        newGrid[index] = newRow;
+      });
+    }
+    setMapState(newGrid);
+  };
+  const moveCharacter = (direction) => {
+    console.log("move character");
+    let temp = {};
+    if (direction == "left") {
+      console.log("left");
+      temp = charLocation();
+      temp[1][0].newx = temp[1][0].oldx - 1;
+      charUpdate(temp);
+      modifyTable(temp);
+    }
+    if (direction == "right") {
+      console.log("Right");
+      temp = {
+        1: [
+          {
+            oldx: 1,
+            newx: 2,
+            tile: <DrawCharacter background={floorSVG()} />,
+          },
+        ],
+      };
+    }
+    if (direction == "down") {
+      //console.log("down");
+      temp = {
+        1: [
+          {
+            oldx: 1,
+            newx: 1,
+            tile: <>{floorSVG()}</>,
+          },
+        ],
+        2: [
+          {
+            oldx: 1,
+            newx: 1,
+            tile: (
+              <>
+                <div style={{ position: "absolute", opacity: "60%" }}>
+                  {floorSVG()}
+                </div>
+                <div style={{ position: "relative" }}>{warriorSVG()}</div>
+              </>
+            ),
+          },
+        ],
+        3: [
+          {
+            oldx: 8,
+            newx: 8,
+            tile: <>{floorSVG()}</>,
+          },
+        ],
+      };
+      temp[2].push({
+        oldx: 8,
+        newx: 7,
+        tile: <DrawMonster background={floorSVG()} />,
+      });
+    }
+    modifyTable(temp);
+  };
+  const newChange = (direction) => {
+    console.log("direction " + direction);
+    let tt = charLocation();
+    {
+      direction;
+    }
+    modifyTable(tt);
+    tt[1][0].oldx = tt[1][0].newx;
+    setChar(tt);
+    console.log("newchange");
+    console.log(tt);
+    //updateCharacter(tt);
+  };
+  return (
+    <div>
+      {mapState.map((row) => (
+        <tr>
+          {row.map((tile) => (
+            <td>{tile}</td>
+          ))}
+        </tr>
+      ))}
+      <td>
+        <button onClick={() => newChange(changeRight())}>right</button>
+      </td>
+      <td>
+        <button onClick={() => newChange(changeLeft())}>left</button>
+      </td>
+      <td>
+        <button onClick={() => moveCharacter("down")}>down</button>
+      </td>
+      <td>
+        <button onClick={() => newChange("up")}>up</button>
+      </td>
+      <td>
+        <button onClick={() => moveMob()}>mob</button>
+      </td>
+      {pagetest}
+    </div>
+  );
 };
