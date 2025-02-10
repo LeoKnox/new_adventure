70d5d4c9ca15797d5f8d49b28414b995047d06c8
LeoKnox <LeoKnox@users.noreply.github.com>
2025-02-09 17:59:14 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -76,38 +76,9 @@ export const changeRight = () => {
   playData["character"] = t;
 };
 
-export const updateMonster = (direction = "up") => {
-  let tempData = playData.monster;
-  if (direction === "left") {
-    tempData = {
-      3: [
-        {
-          oldx: playData.monster[3][0].newx,
-          newx: playData.monster[3][0].newx - 1,
-          tile: <DrawMonster background={floorSVG()} />,
-        },
-      ],
-    };
-  }
-  if (direction === "up") {
-    let a = Object.keys(playData.monster)[0];
-    console.log("up");
-    (tempData[a] = [
-      {
-        oldx: playData.monster[a][0].newx,
-        newx: playData.monster[a][0].newx,
-        tile: floorSVG(),
-      },
-    ]),
-      (tempData[a - 1] = [
-        {
-          oldx: playData.monster[a][0].newx,
-          newx: playData.monster[a][0].newx,
-          tile: <DrawMonster background={floorSVG()} />,
-        },
-      ]),
-      (mobData = tempData);
-  }
+export const updateMonster = (dirx = -1, diry = 0) => {
+  console.log("update monster");
+  dirx && console.log("x = -1");
 };
 
 export const change = () => {
