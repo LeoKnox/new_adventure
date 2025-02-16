71a0e071b74cc03e956139a719f39347abdc44ed
LeoKnox <LeoKnox@users.noreply.github.com>
2025-02-15 18:29:55 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -77,9 +77,9 @@ export const mobDown = (mod) => {
   //return playData["monster"];
   let blank = wallSVG();
   let gg = {
-    3: [{ oldx: 8, newx: 8, tile: [blank] }],
+    3: [{ oldx: 8, newx: 8, tile: <DrawMonster background={floorSVG()} /> }],
 
-    2: [{ oldx: 8, newx: 8, tile: <DrawMonster background={floorSVG()} /> }],
+    2: [{ oldx: 8, newx: 8, tile: [blank] }],
   };
   //gg[2][0].tile = wallSVG();
   return gg;
