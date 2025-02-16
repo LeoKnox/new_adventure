d197fe1b4d8bed858ba07ecf3da6d0a17f4df300
LeoKnox <LeoKnox@users.noreply.github.com>
2025-02-15 18:21:19 -0800

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -108,7 +108,7 @@ export default DrawMap = ({
     modifyTable({
       3: [{ oldx: 8, newx: 7, tile: <DrawMonster background={floorSVG()} /> }],
 
-      2: [{ oldx: 8, newx: 7, tile: {{v}} /> }],
+      2: [{ oldx: 8, newx: 7, tile: {{v}} }],
     });
   };
 
