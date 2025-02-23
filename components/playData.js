86a87732f5e164008a0b76ebb2e85d22f60109cb
LeoKnox <LeoKnox@users.noreply.github.com>
2025-02-22 19:33:37 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -73,7 +73,7 @@ export const mobDown = (mod) => {
   //console.log("md " + (charKey + mod));
   let temp = playData["monster"];
   console.log(temp);
-  temp[parse.Int(charKey) + parse.Int(mod)] = playData["monster"][charKey];
+  temp[parseInt(charKey) + parseInt(mod)] = playData["monster"][charKey];
   console.log(temp);
   temp[charKey][0].tile = wallSVG();
   temp[charKey] = temp;
