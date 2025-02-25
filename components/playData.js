378d9bb4598a74cda516bd6cd180ac801afe57e1
LeoKnox <LeoKnox@users.noreply.github.com>
2025-02-24 18:30:13 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -69,7 +69,10 @@ export const changeLeft = (mod) => {
 export const mobDown = (mod) => {
   console.log("mobDown");
   let charKey = Object.keys(playData["monster"])[0];
-  let temp = structuredClone(playData["monster"]);
+  //let temp = structuredClone(playData["monster"]);
+  let temp = Object.entries(playData["monster"].forEach(key, value) {
+    key == charKey ? {2: value}:{key:value}
+  });
   temp[parseInt(charKey) + parseInt(mod)] = playData["monster"][charKey];
   temp[charKey][0].tile = wallSVG();
   playData["monster"] = temp;
