36075fb0ff9972d02a0f6fcb07832d7e812bcf11
LeoKnox <LeoKnox@users.noreply.github.com>
2025-03-02 17:42:20 -0800

    Update PlayMap.js

--- components/PlayMap.js
+++ components/PlayMap.js
@@ -1,30 +1,110 @@
-import { useState } from "react";
-import { floorSVG, wallSVG, warriorSVG } from "./svgData";
-import { singleRoom } from "./dungeonData.js";
-import DrawCharacer from "./DrawCharacter.js";
-import DrawMap from "./DrawMap.js";
+import { floorSVG, wallSVG, warriorSVG, warr } from "./svgData";
+import DrawMonster from "./DrawMonster.js";
+import DrawCharacter from "./DrawCharacter.js";
 
-export default PlayMap = () => {
-  let newMap = singleRoom(1);
-  const [test, setTest] = useState([
-    [wallSVG(), wallSVG()],
-    [wallSVG(), wallSVG()],
-  ]);
-  const [x, setx] = useState(1);
-  const [y, sety] = useState(1);
+let playData = {
+  character: {
+    1: [{ oldx: 3, newx: 3, tile: <DrawCharacter background={floorSVG()} /> }],
+  },
+  monster: {
+    3: [{ oldx: 8, newx: 8, tile: <DrawMonster background={floorSVG()} /> }],
+  },
+};
+let mobData = {};
+let currMap = [
+  [floorSVG(), floorSVG()],
+  [floorSVG(), floorSVG()],
+];
 
-  return (
-    <>
-      <table>
-        <DrawMap
-          width={newMap.width}
-          height={newMap.height}
-          x={x}
-          y={y}
-          sety={sety}
-          setx={setx}
-        />
-      </table>
-    </>
+mobData = {
+  3: [{ oldx: 8, newx: 8, tile: warr }],
+  //3: [{ oldx: 8, newx: 8, tile: <DrawMonster background={floorSVG()} /> }],
+};
+
+export const mapData = currMap;
+
+export const addChar = () => {
+  temp = currMap;
+  temp[0][0] = warriorSVG();
+  return temp;
+};
+
+export const move = () => {
+  currMap[1][0] = wallSVG();
+  return currMap;
+};
+
+export const charUpdate = (
+  newChar = {
+    1: [{ oldx: 3, newx: 2, tile: <DrawCharacter background={floorSVG()} /> }],
+  }
+) => {
+  console.log("charUpdate");
+  playData.character = newChar;
+  let temp = newChar;
+  playData.character[1][0].oldx = playData.char[1][0].newx;
+};
+
+export const updateCharacter = (newData) => {
+  console.log("update character");
+  playData["character"] = newData;
+  console.log(newData);
+};
+export const charLocation = (id = 1) => {
+  console.log("char loc");
+  return playData["character"];
+};
+export const changeDown = (direction = 1) => {
+  console.log("change down");
+  console.log(playData["character"]);
+  let charKey = parseInt(Object.keys(playData["character"]));
+  let t = deepCopy(playData["monster"]);
+  t[charKey][0]["tile"] = floorSVG();
+  console.log(t);
+  playData["monster"] = t;
+  //delete t[charKey];
+};
+export const deleteDown = (offSet = 1) => {
+  console.log("delete down");
+  let charKey = parseInt(Object.keys(playData["character"]) + offSet);
+  delete playData["character"][charKey];
+  console.log(playData["character"]);
+};
+export const changeLeft = (mod) => {
+  let charKey = +Object.keys(playData["monster"])[0];
+  playData["monster"][charKey][0].oldx = playData["monster"][charKey][0].newx;
+  playData["monster"][charKey][0].newx =
+    playData["monster"][charKey][0].newx + mod;
+  return playData["monster"];
+};
+export const mobDown = (mod) => {
+  console.log("mobDown");
+  let charKey = Object.keys(playData["monster"])[0];
+  //let temp = structuredClone(playData["monster"]);
+  let temp = playData["monster"][charKey][0];
+  console.log(temp);
+  temp[parseInt(charKey) + parseInt(mod)] = structuredClone(
+    playData["monster"][charKey]
   );
+  temp[charKey][0].tile = { warr };
+  playData["monster"] = temp;
+  console.log(playData["monster"]);
+};
+
+export const updateMonster = (dirx, diry = -1) => {
+  console.log("update monster");
+  dirx && changeLeft(dirx);
+  diry && mobDown(diry);
+  return playData["monster"];
+};
+
+export const change = () => {
+  console.log("change");
+  console.log(playData["character"]);
+  return playData["character"];
+};
+
+export const singleMonster = () => {
+  console.log("mob data");
+  return mobData;
 };
