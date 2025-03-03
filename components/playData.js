b4f5377377972ecad29e501cbf78b4fb8e44ebc2
LeoKnox <LeoKnox@users.noreply.github.com>
2025-03-02 18:06:45 -0800

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -26,7 +26,7 @@ const newMap = () => {
   let x = [];
   let y = [];
   for (let i = 0; i < ourX; i++) {
-    x.push(i);
+    x.push([floorSVG(), floorSVG()]);
   }
   return x;
 };
@@ -36,7 +36,7 @@ export const mapData = newMap();
 export const addChar = (i = 0) => {
   console.log("map data");
   console.log(mapData);
-  let temp = currMap;
+  let temp = newMap;
   temp[i][0] = warriorSVG();
   return temp;
 };
