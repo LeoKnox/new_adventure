df84ef33c480fa0b5bf77a76b4d65379bab24c07
LeoKnox <LeoKnox@users.noreply.github.com>
2025-03-12 19:11:50 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -63,8 +63,7 @@ export default DrawMap = ({
     console.log("locations");
     console.table(currMap);
     temp[locations.y][locations.x] = warriorSVG();
-    setDataMove([...temp]);
-    //console.log("set");
+    setDataMove(temp);
   }, [locations]);
 
   useEffect(() => {
