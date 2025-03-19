ff7a222f90f05a2fdbc8a7c33d182d4948c64b63
LeoKnox <LeoKnox@users.noreply.github.com>
2025-03-18 18:33:46 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -78,10 +78,10 @@ export default DrawMap = ({
   const createWalls = (count) => {
     const elements = [];
     const rowTemp = [];
-    rowTemp.push(<tr)>
+    rowTemp.push(<tr>
     for (let i = 0; i < count; i++) {
       rowTemp.push(<td>{wallSVG()}</td>)
-    }
+    }</tr>)
     return elements;
   };
 
