3476352ec53bcce0cf53d61cb4886bdbb0ca421e
LeoKnox <LeoKnox@users.noreply.github.com>
2025-03-22 15:17:09 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -78,7 +78,7 @@ export default DrawMap = ({
   const createWalls = () => {
     console.log("create walls");
     let z = 1;
-    let elements = dataMove.map((s, t) => (t == z ? console.log(typeof s) : s));
+    let elements = dataMove.map((s, t) => (t == z ? s.map((v) => console.log(v)) : s));
     //elements[1][1] = <td id="*">{warriorSVG()}</td>;
     console.log(elements);
     /*
