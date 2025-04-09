8bf132c44c5b1673b6bdb7cd0d937b1823add38c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-08 20:33:21 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -79,6 +79,103 @@ export default DrawMap = ({
     let temp = { ...mabData };
     Object.keys(temp).forEach(function (data, index) {
       console.log(temp[data][index]);
+      console.log(data);
+      console.log(index);
+    });
+    //console.log(temp);
+    setMabData(temp);
+  };
+  const createWalls = useCallback(() => {
+    console.log("create walls");
+    let cw = dataMove.map((s, t) => (
+      <tr>
+        {s.props.children.map((u, v) => (
+          <td>{confirm(t, v) || u}</td>
+        ))}
+      </tr>
+    ));
+    return cw;import { useState, useEffect, useCallback, cloneElement } from "react";
+import { demonSVG, floorSVG, wallSVG, warriorSVG, warr } from "./svgData";
+import { singleRoom } from "./dungeonData.js";
+import {
+  charLocation,
+  updateMonster,
+  mobData,
+  mapData,
+  move,
+} from "./playData.js";
+import DrawMonster from "./DrawMonster.js";
+import DrawCharacter from "./DrawCharacter.js";
+import { textSpanOverlap } from "typescript";
+
+export default DrawMap = ({
+  width = 10,
+  height = 10,
+  x = 2,
+  y = 3,
+  sety,
+  setx,
+}) => {
+  const currMap = new Array(singleRoom().x)
+    .fill()
+    .map(() => <tr>{Array(singleRoom().y).fill(<td>{floorSVG()}</td>)}</tr>);
+  const [dataMove, setDataMove] = useState(currMap);
+  const [locations, setLocations] = useState(mapData);
+  const [mabData, setMabData] = useState(mobData);
+  const [mapState, setMapState] = useState(() => {
+    let temp = [];
+    let tempRow = [];
+    for (let x = 0; x < width + 2; x++) {
+      tempRow.push(wallSVG());
+    }
+    temp.push(tempRow);
+    tempRow = [];
+    for (let x = 0; x < height; x++) {
+      tempRow.push(wallSVG());
+      for (let y = 0; y < width; y++) {
+        tempRow.push(floorSVG());
+      }
+      tempRow.push(wallSVG());
+      temp.push(tempRow);
+      tempRow = [];
+    }
+    for (let x = 0; x < width + 2; x++) {
+      tempRow.push(wallSVG());
+    }
+    temp.push(tempRow);
+    tempRow = [];
+    return temp;
+  });
+
+  useEffect(() => {
+    console.log("locations");
+    let temp = [...currMap];
+    temp[locations["player"].y][locations["player"].x] = warriorSVG();
+    locations["monster"].map((mon) => (temp[mon.y][mon.x] = demonSVG()));
+    setDataMove(temp);
+  }, [locations]);
+
+  const moveMob = (xval = -1) => {
+    console.log("move mob");
+    let change = updateMonster(0, -1);
+    modifyTable(change);
+  };
+
+  const confirm = (t, v) => {
+    console.log("confirm");
+    if (mabData.hasOwnProperty(t) && mabData[t].hasOwnProperty(v)) {
+      return mabData[t][v];
+    } else {
+      return false;
+    }
+  };
+  const newMove = (x, y) => {
+    console.log("newmove");
+    //setMabData(move(x, y, mabData));
+    let temp = { ...mabData };
+    Object.keys(temp).forEach(function (data, index) {
+      console.log(temp[data][index]);
+      console.log(data);
       console.log(index);
     });
     //console.log(temp);
@@ -94,6 +191,30 @@ export default DrawMap = ({
       </tr>
     ));
     return cw;
+  }, [dataMove, mabData]);
+
+  return (
+    <div>
+      <label>
+        <button onClick={() => newMove(1, 0)}>right</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(-1, 0)}>left</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(0, 1)}>down</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(0, -1)}>up</button>
+      </label>
+      <label>
+        <button onClick={() => newMove(0, 0)}>mob</button>
+      </label>
+      <table>{createWalls()}</table>
+    </div>
+  );
+};
+
   }, [dataMove, mabData]);
 
   return (
