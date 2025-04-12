4e4a0bc1e0f27685cdc9c5f588fe087d611b80d5
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-12 16:38:34 -0700

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -63,7 +63,7 @@ export const move = (x = 1, y = 0, currData) => {
   console.log("next move");
   console.log(loc[0]);
   if (currData[loc[0]] != 0) {
-    currData[loc[0] + x] = { [+loc[0]]: warriorSVG() };
+    currData[+loc[0] + x] = { [+loc[0]]: warriorSVG() };
   }
   return currData;
 };
