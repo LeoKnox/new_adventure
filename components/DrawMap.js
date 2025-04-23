0e14cd1b94a69a76d8fea6c6632bee705039e53b
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-22 19:28:54 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -102,7 +102,7 @@ export default DrawMap = ({
     (rows = 4, columns = 4, defaultValue = "*") => {
       console.log("bottom layer");
       const blTemp = Array.from({ length: rows }, () =>
-        Array.from({ length: columns }, () => defaultValue)
+        Array.from({ length: columns }, () => <td>{defaultValue}</td>)
       );
       return blTemp;
     },
