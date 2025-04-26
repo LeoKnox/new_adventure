bab958442ea58c1452f33f98c9cf2413f19d16bc
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-25 19:31:58 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -128,7 +128,7 @@ export default DrawMap = ({
       const blTemp = Array.from({ length: rows }, (t, i) => (
         <tr>
           {Array.from({ length: columns }, (u, j) =>
-            findPos(u, j) ? <td>{defaultValue}</td> : <td></td>
+            findPos(i, j) ? <td>{defaultValue}</td> : <td></td>
           )}
         </tr>
       ));
