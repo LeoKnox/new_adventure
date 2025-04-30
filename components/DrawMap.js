64485135feb6ef482aa507264172d62595e75d1e
LeoKnox <LeoKnox@users.noreply.github.com>
2025-04-30 14:19:40 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -45,7 +45,7 @@ export default DrawMap = ({
     //console.log(temp);
     //console.log(charPos);
     let tempKey = x + ":" + y;
-    if (typeof charPos[0][temp] != "undefined") {
+    if (charPos[0][temp]?) {
       console.log(charPos[0][temp]);
       return charPos[0][temp];
     }
