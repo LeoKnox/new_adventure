c90de0a68de8bcefd4a05311d1cac67be9ecfbeb
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-14 11:24:32 -0700

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -160,7 +160,7 @@ export const changePlayer = (charPos, x, y) => {
   let temp = {};
   Object.entries(charPos).map(([key]) => {
     let tempKeys = key.split(":");
-    let temp = { [`${x + +tempKeys[0]}:${y + +tempKeys[1]}`]: charFunc[0] };
+    temp = { [`${x + +tempKeys[0]}:${y + +tempKeys[1]}`]: charFunc[0] };
   });
   console.log(temp);
   return temp;
