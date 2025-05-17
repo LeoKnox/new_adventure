d626d1ebdc15ba7a196f12b3b22e5d20d157c8ed
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-17 14:12:52 -0700

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import DrawMap from "./DrawMap.js";
 import PlayMap from "./PlayMap.js";
+import { singleRoom } from "./dungeonData.js";
 
 export default Play = () => {
+  let newMap = singleRoom(1);
   return (
     <>
       <h2>Play game</h2>
       <table>
-        <DrawMap />
+        <DrawMap width={newMap.width} height={newMap.height} />
       </table>
     </>
   );
