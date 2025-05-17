ca59866b26de17079bd1e2184978a71af9852f49
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-17 14:07:41 -0700

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
+import DrawMap from "./DrawMap.js";
 import PlayMap from "./PlayMap.js";
 
 export default Play = () => {
   return (
     <>
       <h2>Play game</h2>
-      <>
-        <p>Game!</p>
-        <PlayMap />
-      </>
+      <table>
+        <DrawMap />
+      </table>
     </>
   );
 };
