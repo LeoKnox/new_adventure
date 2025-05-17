6e2abb9f3c1d1336e645bcf78b6d46a53b0a14e8
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-17 14:04:53 -0700

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -1,26 +1,14 @@
 import { useState } from "react";
-import { allCharacters } from "./characterData.js";
-import { charLocation } from "./playData";
 import PlayMap from "./PlayMap.js";
-import SelectCharacter from "./SelectCharacter.js";
-import View from "./View.js";
 
 export default Play = () => {
-  const [playerId, setPlayerId] = useState(1);
-  let playerLocation = charLocation();
-  console.log(playerLocation);
   return (
     <>
       <h2>Play game</h2>
-      {playerId}
-      {playerId <= 0 ? (
-        <SelectCharacter setPlayerId={setPlayerId} />
-      ) : (
-        <>
-          <p>Game!</p>
-          <PlayMap />
-        </>
-      )}
+      <>
+        <p>Game!</p>
+        <PlayMap />
+      </>
     </>
   );
 };
