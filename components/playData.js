c136ded878284e6f42dba4415475cd3ead450c96
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-23 20:32:16 -0700

    Update playData.js

--- components/playData.js
+++ components/playData.js
@@ -16,8 +16,91 @@ const charFunc = { 0: warriorSVG(), 1: demonSVG() };
 export const playerPos = { [`3:1`]: charFunc[0] };
 export const mobPos = { [`1:4`]: charFunc[1], ["1:5"]: charFunc[1] };
 
-const mobDirection = () => {
-  return [9, 9];
+const mobDirection = (key) => {
+  let tempKeys = key.split(":");
+  let newY = 0;
+  let newX = 0;
+  const [tempx, tempy] = mobDirection();
+  console.log(key);
+  //console.log(tempKeys[1] + "::" + charChange[1]);
+  console.log(tempy + "::" + tempx);
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[0] == charChange[0] &&
+    tempKeys[1] > charChange[1]
+  ) {
+    console.log("a");
+    newY = -1;
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[0] == charChange[0] &&
+    tempKeys[1] < charChange[1]
+  ) {
+    console.log("b");
+    newY = 1;
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[1] == charChange[1] &&
+    tempKeys[0] < charChange[0]
+  ) {
+    console.log("c");
+    newX = -1;
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[1] == charChange[1] &&
+    tempKeys[0] > charChange[0]
+  ) {
+    console.log("d");
+    newX = 1;
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[0] > charChange[0] &&
+    tempKeys[1] > charChange[1]
+  ) {
+    console.log("e");
+    Math.round(Math.random()) ? (newY = 1) : (newX = 1);
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[0] < charChange[0] &&
+    tempKeys[1] < charChange[1]
+  ) {
+    console.log("f");
+    Math.round(Math.random()) ? (newY = -1) : (newX = -1);
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[0] > charChange[0] &&
+    tempKeys[1] < charChange[1]
+  ) {
+    console.log("g");
+    Math.round(Math.random()) ? (newY = 1) : (newX = -1);
+  }
+  if (
+    !newX &&
+    !newY &&
+    tempKeys[0] < charChange[0] &&
+    tempKeys[1] > charChange[1]
+  ) {
+    console.log("h");
+    Math.round(Math.random()) ? (newY = 1) : (newX = 1);
+  }
+  temp = {
+    ...temp,
+    [`${+tempKeys[0] + newX}:${+tempKeys[1] + newY}`]: charFunc[1],
+  };
+  return temp;
 };
 
 export const changeMob = (evilPos, x, y, charPos) => {
@@ -25,6 +108,8 @@ export const changeMob = (evilPos, x, y, charPos) => {
   let temp = {};
   let charChange = Object.keys(charPos)[0].split(":");
   Object.entries(evilPos).map(([key]) => {
+    const temp = mobDirection(key);
+    /*
     let tempKeys = key.split(":");
     let newY = 0;
     let newX = 0;
@@ -107,7 +192,7 @@ export const changeMob = (evilPos, x, y, charPos) => {
     temp = {
       ...temp,
       [`${+tempKeys[0] + newX}:${+tempKeys[1] + newY}`]: charFunc[1],
-    };
+    };*/
   });
   return temp;
 };
