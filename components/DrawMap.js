c9d79e8b3dd4dc2902e8a6307246b99c96b6013a
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-26 15:44:05 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -42,7 +42,7 @@ export default DrawMap = ({ height = 10, width = 10 }) => {
 
   const changeMap = (x, y) => {
     let temp = changePlayer(charPos, x, y);
-    evilPos[Object.keys(temp)[0]] ? alert("true") : alert("false");
+    evilPos[Object.keys(temp)[0]] ? alert("true") : null;
     setCharPos(temp);
     setEvilPos(changeMob(evilPos, x, y, charPos));
   };
