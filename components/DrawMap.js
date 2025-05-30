d8cee42d43aef86a22ee47338ce4c8355ceb8019
LeoKnox <LeoKnox@users.noreply.github.com>
2025-05-30 13:38:40 -0700

    Update DrawMap.js

--- components/DrawMap.js
+++ components/DrawMap.js
@@ -11,6 +11,9 @@ export default DrawMap = ({ height = 10, width = 10 }) => {
   const [currRoom, setCurrRoom] = useState(1);
   height = singleRoom(currRoom).height;
   width = singleRoom(currRoom).width;
+  let [monsters] = singleRoom(currRoom);
+  console.log("monsters");
+  console.log(monsters);
   const bottomLayer = useCallback(
     (rows = height, columns = width, defaultValue = floorSVG()) => {
       console.log("bottom layer");
