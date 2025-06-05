b95ae120a2b76e1cc67daee39ac76025ff29854c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-05 15:50:41 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -19,7 +19,8 @@ export default Build = () => {
     console.log("edit func");
     console.log(roomEdit);
     const temp = changeRoom(roomEdit);
-    temp.monsters = { ...temp.monsters, [`2:3`]: 1 };
+    temp[newId].monsters = { ...temp.monsters, [`2:3`]: 1 };
+    console.log(temp);
     setRooms(temp);
     setIsEdit(false);
   };
