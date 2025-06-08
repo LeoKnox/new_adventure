e7a32da71859aea789b5d0787ae4fcd1aa9e9c7b
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-07 19:11:25 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -19,7 +19,7 @@ export default Build = () => {
     console.log("edit func");
     console.log(roomEdit);
     let temp = changeRoom(roomEdit);
-    let tempMob = temp.monsters;
+    let tempMob = changeRoom(roomEdit).monsters;
     console.log(tempMob);
     tempMob = { ...tempMob, [`2:3`]: 1 };
     temp.monsters = tempMob;
