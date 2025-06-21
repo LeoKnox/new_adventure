e440de395f6d59892d5a1489ab3e1140407cbc4d
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-21 16:44:07 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -1,6 +1,6 @@
-export default RoomMonsters = (room, roomEdit, setRoomEdit) => {
+export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
   console.log("room");
-  console.log(room);
+  console.log(room[`1:4`]);
   return (
     <>
       <p>room monsters</p>
