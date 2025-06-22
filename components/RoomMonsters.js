b88b4ff3cc60a42c8dfaa3cc550e17dc3b37e079
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-22 14:59:39 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -1,16 +1,163 @@
-export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
-  console.log("room");
-  console.table(room);
-  const deleteMonster = () => {
+import { useState, useCallback } from "react";
+import RoomMonsters from "./RoomMonsters.js";
+import { singleRoom, changeRoom, allRooms } from "./dungeonData.js";
+
+export default EditRoom = ({
+  newId = 1,
+  setIsEdit,
+  rooms,
+  setRooms,
+  editFunc,
+  removeMob,
+}) => {
+  console.log("edit room");
+  const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
+  const [newMob, setNewMob] = useState({ x: 0, y: 0 });
+  const submitRoom = () => {
+    console.log("submit room");
+    let temp = rooms.findIndex((room) => room.id === newId);
+    temp >= 0 && (rooms[temp] = roomEdit);
+    setRooms(rooms);
+    setIsEdit(false);
+  };
+  const deleteMob = (index) => {
+    console.log("delete mob");
+    console.log(index);
     let temp = roomEdit;
-    delete roomEdit.monsters[`1:4`];
+    console.log(temp);
+    delete temp.monsters[index];
+    console.log(temp);
     setRoomEdit(temp);
   };
+
+  const splitXY = useCallback(
+    (key, index) => {
+      console.log("split xy");
+      console.log(index);
+      let [x, y] = key.split(":");
+      return (
+        <>
+          <label>
+            monster x:
+            <input type="number" value={x} />
+          </label>
+          <label>
+            monster y:
+            <input type="number" value={y} />
+          </label>
+          <button
+            onClick={() => {
+              deleteMob(key);
+            }}
+          >
+            X
+          </button>
+        </>
+      );
+    },
+    [roomEdit, deleteMob]
+  );
+  const listMobs = useCallback(() => {
+    let temp = [];
+    Object.keys(roomEdit.monsters).map((key, index) =>
+      temp.push(
+        <p className="mobInput">
+          {splitXY(key, index)}:{key}
+        </p>
+      )
+    );
+    console.log(temp);
+    return temp;
+  }, [roomEdit, deleteMob]);
   return (
     <>
-      <p>room monsters</p>
-      <button onClick={() => deleteMonster()}>update</button>
-      {JSON.stringify(roomEdit)}
+      <button>{newId}</button>
+      <p>{singleRoom(newId).name}</p>
+      <p>
+        Name:
+        <input
+          type="text"
+          value={roomEdit.name}
+          onChange={(e) => setRoomEdit({ ...roomEdit, name: e.target.value })}
+        />
+      </p>
+      <p>
+        Width:
+        <input
+          type="number"
+          value={roomEdit.width}
+          onChange={(e) => setRoomEdit({ ...roomEdit, width: e.target.value })}
+        />
+      </p>
+      <p>
+        Height:
+        <input
+          type="number"
+          value={roomEdit.height}
+          onChange={(e) => setRoomEdit({ ...roomEdit, height: e.target.value })}
+        />
+      </p>
+      <p>
+        X:
+        <input
+          type="number"
+          value={roomEdit.x}
+          onChange={(e) => setRoomEdit({ ...roomEdit, x: e.target.value })}
+        />
+      </p>
+      <p>
+        Y:
+        <input
+          type="number"
+          value={roomEdit.y}
+          onChange={(e) => setRoomEdit({ ...roomEdit, y: e.target.value })}
+        />
+      </p>
+      <RoomMonsters
+        roomEdit={roomEdit}
+        setRoomEdit={setRoomEdit}
+        room={{
+          id: 2,
+          name: "entry",
+          width: 7,
+          height: 8,
+          x: 6,
+          y: 6,
+          monsters: {
+            [`1:4`]: 1,
+            [`1:5`]: 1,
+          },
+        }}
+      />
+      {/*Object.keys(roomEdit.monsters).map((key, index) => (
+        <p className="mobInput">
+          {splitXY(key, index)}:{key}
+        </p>
+      ))*/}
+      <p>
+        <label>
+          New Mob X
+          <input
+            type="number"
+            className="mobInput"
+            value={newMob.x}
+            onChange={(e) => setNewMob({ ...newMob, y: e.target.value })}
+          />
+        </label>
+        <label>
+          New Mob Y
+          <input
+            type="number"
+            className="mobInput"
+            value={newMob.y}
+            onChange={(e) => setNewMob({ ...newMob, x: e.target.value })}
+          />
+        </label>
+        <button onClick={() => editFunc(roomEdit, newMob)}>create</button>
+      </p>
+      <button onClick={() => submitRoom()}>Submit</button>
+      <button onClick={() => setIsEdit(false)}>Back</button>
+      <p>{JSON.stringify(roomEdit)}</p>
     </>
   );
 };
