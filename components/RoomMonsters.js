1c174d33676b2db0246c9e9ec56f0ca0089198c9
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-24 18:56:22 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -7,7 +7,59 @@ export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
     let tempMobs = { ...temp.monsters };
     delete tempMobs[`1:4`];
     temp.monsters = tempMobs;
+    setRoomEdit(temp);import {useState} from "react"
+
+export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
+  console.log("room");
+  const deleteMonster = (value) => {
+    let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    delete tempMobs[value];
+    temp.monsters = tempMobs;
     setRoomEdit(temp);
+  };
+  return (
+    <>
+      <p>room monsters</p>
+      <button onClick={() => deleteMonster()}>update</button>
+      {Object.entries(roomEdit["monsters"]).map(([key, value]) => (
+        <p>import {useState} from "react"
+
+export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
+  console.log("room");
+  const deleteMonster = (value) => {
+    let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    delete tempMobs[value];
+    temp.monsters = tempMobs;
+    setRoomEdit(temp);
+  };
+  return (
+    <>
+      <p>room monsters</p>
+      <button onClick={() => deleteMonster()}>update</button>
+      {Object.entries(roomEdit["monsters"]).map(([key, value]) => (
+        <p>
+          <label>Mob X<input type="number" value={key.split(":")[0]}/></label>
+          <label>Mob Y<input type="number" value={key.split(":")[1]}/></label>
+          <label>Mob type<input type="number" value={value} /></label>
+          <button onClick={()=>deleteMonster(value)}>X</button>
+        </p>
+      ))}
+    </>
+  );
+};
+
+          <label>Mob X<input type="number" value={key.split(":")[0]}/></label>
+          <label>Mob Y<input type="number" value={key.split(":")[1]}/></label>
+          <label>Mob type<input type="number" value={value} /></label>
+          <button onClick={()=>deleteMonster(value)}>X</button>
+        </p>
+      ))}
+    </>
+  );
+};
+
   };
   return (
     <>
