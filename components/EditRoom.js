cf19d9a5322366f1915aa07f125a78e5e4a1d316
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-25 13:39:40 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -21,8 +21,8 @@ export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
       <button onClick={() => deleteMonster()}>update</button>
       {Object.entries(mobValues).map(([key, value]) => (
         <p>
-          <label>Mob X<input type="number" onChange={(e) => updateMob(key)} value={key.split(":")[0]}/></label>
-          <label>Mob Y<input type="number" value={key.split(":")[1]}/></label>
+          <label>Mob X<input type="number" onChange={(e) => updateMob(key)} value={value[0].split(":")[0]}/></label>
+          <label>Mob Y<input type="number" value={value[0].split(":")[1]}/></label>
           <label>Mob type<input type="number" value={value} /></label>
           <button onClick={()=>deleteMonster(key)}>X</button>
         </p>
