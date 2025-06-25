dee5b67ffdbccc3776c95e669e130dc29f24225b
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-25 15:22:04 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -5,7 +5,43 @@ export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
   const [mobValues, setMobValues] = useState(Object.entries(roomEdit.monsters))
   console.table(mobValues);
   const deleteMonster = (key) => {
+    let temp = { ...roomEdit };import {useState} from "react"
+
+export default RoomMonsters = ({ room, roomEdit, setRoomEdit }) => {
+  console.log("room");
+  const [mobValues, setMobValues] = useState(Object.entries(roomEdit.monsters))
+  console.table(mobValues);
+  const deleteMonster = (key) => {
     let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    delete tempMobs[key];
+    temp.monsters = tempMobs;
+    setRoomEdit(temp);
+  };
+  const updateMob=(key)=> {
+    console.log("update mob");
+    console.log(key);
+  }
+  return (
+    <>
+      <p>room monsters</p>
+      <button onClick={() => deleteMonster()}>update</button>
+      {Object.entries(mobValues).map(([key, value]) => (
+        <p>
+          <label>Mob X<input type="number" onChange={(e) => setMobValues((prevMob) => {
+      const result = [...prevMob];
+      result[index] = e.target.value;
+      return result})} value={value[0].split(":")[0]}/></label>
+          <label>Mob Y<input type="number" value={value[0].split(":")[1]}/></label>
+          <label>Mob type<input type="number" value={value} /></label>
+          <button onClick={()=>deleteMonster(key)}>X</button>
+        </p>
+      ))}
+      {JSON.stringify(mobValues)}
+    </>
+  );
+};
+
     let tempMobs = { ...temp.monsters };
     delete tempMobs[key];
     temp.monsters = tempMobs;
