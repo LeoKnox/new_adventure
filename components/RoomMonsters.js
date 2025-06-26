1ea0f1625b7edc08d2bb5b69b5e04e462262d552
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-26 14:05:25 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -19,13 +19,13 @@ export default RoomMonsters = ({ room, editMobs, roomEdit, setRoomEdit }) => {
     <>
       <p>room monsters</p>
       <button onClick={() => deleteMonster()}>update</button>
-      {Object.entries(mobValues).map(([key, value]) => (
+      {Object.entries(roomEdit.monsters).map(([key, value]) => (
         <p>
           <label>Mob X<input type="number" onChange={(e) => editMobs(key, value)}/></label>
-          <label>Mob Y<input type="number" onChange={(e) => setMobValues((prevMob) => {
+          {/*<label>Mob Y<input type="number" onChange={(e) => setMobValues((prevMob) => {
       const result = [...prevMob];
       result[key] = e.target.value;
-      return result})}  value={value[0].split(":")[1]}/></label>
+      return result})}  value={value[0].split(":")[1]}/></label>*/}
           <label>Mob type<input type="number" value={value[1]} /></label>
           <button onClick={()=>deleteMonster(key)}>X</button>
         </p>
