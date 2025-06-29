d71c88f6c4595e739e8167f1f11fe2bea608934f
LeoKnox <LeoKnox@users.noreply.github.com>
2025-06-29 14:36:27 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -1,68 +1,17 @@
-import {createContext, useContext, useState} from "react"
-import {setMob} from "./MobContext.js";
+import { createContext, useContext, useState } from "react";
 
-export default RoomMonsters = ({ room, editMobs, roomEdit, setRoomEdit }) => {
-  console.log("room");
-  const [mobValues, setMobValues] = useState(Object.entries(roomEdit.monsters))
-  //const [newMob, setNewMob] = useState({ x: 0, y: 0 });
-  const { newMob, setMob } = setMob();
-  console.table(mobValues);
-  const deleteMonster = (key) => {
-    let temp = { ...roomEdit };
-    let tempMobs = { ...temp.monsters };
-    delete tempMobs[key];
-    temp.monsters = tempMobs;
-    setRoomEdit(temp);
-  };
-  const addMob=(newMob)=> {
-    console.log("add mob");
-    console.log(newMob);
-    let temp = { ...roomEdit };
-    let tempMobs = { ...temp.monsters };
-    tempMobs[`${newMob.y}"{newMob.x}`] = 1;
-    setRoomEdit(temp);
-  }
+const MobContext = createContext();
+
+export const mobProvider = ({ children }) => {
+  const [newMob, setNewMob] = useState({ [`3:4`]: 1 });
+  const setMob = () => setConMob({ [`4:5`]: 1 });
+  //const incrementCount = () => setCount((prevCount) => prevCount + 1);
+
   return (
-    <>
-      <p>room monsters</p>
-      <button onClick={() => deleteMonster()}>update</button>
-      {Object.entries(mobValues).map(([key, value]) => (
-        <p>
-          <label>Mob X<input type="number" onChange={(e) => editMobs(key, value)}/></label>
-          <label>Mob Y<input type="number" onChange={(e) => setMobValues((prevMob) => {
-      const result = [...prevMob];
-      result[key] = e.target.value;
-      return result})}  value={value[0].split(":")[1]}/></label>
-          <label>Mob type<input type="number" value={value[1]} /></label>
-          <button onClick={()=>deleteMonster(key)}>X</button>
-        </p>
-      ))}
-      {JSON.stringify(mobValues)}
-      <p>
-        <label>
-          New Mob X
-          <input
-            type="number"
-            className="mobInput"
-            value={newMob.x}
-            onChange={(e) => setNewMob({ ...newMob, x: e.target.value })}
-          />
-        </label>
-        <label>
-          New Mob Y
-          <input
-            type="number"
-            className="mobInput"
-            value={newMob.y}
-            onChange={(e) => setNewMob({ ...newMob, y: e.target.value })}
-          />
-        </label>
-        <button onClick={() => addMob(newMob)}>create mob</button>
-        <p>
-        {mobText}
-        <button onClick={setMob}>context mob</button>
-        </p>
-      </p>
-    </>
+    <MobContext.Provider value={{ newMob, setMob }}>
+      {children}
+    </MobContext.Provider>
   );
 };
+
+export const setConMob = () => useContext(MobContext);
