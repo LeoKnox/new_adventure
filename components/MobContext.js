2f4f7aac90491437343e367bd6dcc9617368e0e2
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-02 16:55:30 -0700

    Update MobContext.js

--- components/MobContext.js
+++ components/MobContext.js
@@ -2,6 +2,14 @@ import { createContext, useContext, useState } from "react";
 
 export const MobContext = createContext();
 
-export const updateMob = () => {
+export const useMob = () => {
   return useContext(MobContext);
+};
+
+export const updateMob = () => {
+  const [doors, setDoors] = useState(0);
+
+  const increaseDoors = () => {
+    setDoors(doors + 1);
+  };
 };
