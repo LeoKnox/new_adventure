3e1406ec83c1700db3117518f620d22d7c9d8206
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-03 15:07:35 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -15,7 +15,203 @@ export default EditRoom = ({
   const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
   const [testMob, setTestMob] = useState("mob text");
 
+  const submitRoom = () => {import { createContext, useContext, useState, useCallback } from "react";
+import { useMob } from "./MobContext.js";
+import RoomMonsters from "./RoomMonsters.js";
+import { singleRoom, changeRoom, allRooms } from "./dungeonData.js";
+
+export default EditRoom = ({
+  newId = 1,
+  setIsEdit,
+  rooms,
+  setRooms,
+  editFunc,
+  removeMob,
+}) => {
+  console.log("edit room");
+  const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
+  const [testMob, setTestMob] = useState("mob text");
+
   const submitRoom = () => {
+    console.log("submit room");
+    let temp = rooms.findIndex((room) => room.id === newId);
+    temp >= 0 && (rooms[temp] = roomEdit);
+    setRooms(rooms);
+    setIsEdit(false);
+  };
+
+  const editMobs = (key, value) => {
+    console.log("edit mobs");
+    console.log(key + "&" + value);
+    setRoomEdit({
+      id: 2,
+      name: "entry",
+      width: 7,
+      height: 8,
+      x: 6,
+      y: 6,
+      monsters: {
+        [`2:3`]: 1,
+        [`3:4`]: 1,
+      },
+    });
+  };
+
+  return (
+    <>
+      <button>{newId}</button>
+      <p>{singleRoom(newId).name}</p>
+      <p>
+        Name:
+        <input
+          type="text"
+          value={roomEdit.name}
+          onChange={(e) => setRoomEdit({ ...roomEdit, name: e.target.value })}
+        />
+      </p>
+      <p>
+        Width:
+        <input
+          type="number"
+          value={roomEdit.width}
+          onChange={(e) => setRoomEdit({ ...roomEdit, width: e.target.value })}
+        />
+      </p>
+      <p>
+        Height:
+        <input
+          type="number"
+          value={roomEdit.height}
+          onChange={(e) => setRoomEdit({ ...roomEdit, height: e.target.value })}
+        />
+      </p>
+      <p>
+        X:
+        <input
+          type="number"import { createContext, useContext, useState, useCallback } from "react";
+import { useMob } from "./MobContext.js";
+import RoomMonsters from "./RoomMonsters.js";
+import { singleRoom, changeRoom, allRooms } from "./dungeonData.js";
+
+export default EditRoom = ({
+  newId = 1,
+  setIsEdit,
+  rooms,
+  setRooms,
+  editFunc,
+  removeMob,
+}) => {
+  console.log("edit room");
+  const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
+  const [testMob, setTestMob] = useState("mob text");
+
+  const submitRoom = () => {
+    console.log("submit room");
+    let temp = rooms.findIndex((room) => room.id === newId);
+    temp >= 0 && (rooms[temp] = roomEdit);
+    setRooms(rooms);
+    setIsEdit(false);
+  };
+
+  const editMobs = (key, value) => {
+    console.log("edit mobs");
+    console.log(key + "&" + value);
+    setRoomEdit({
+      id: 2,
+      name: "entry",
+      width: 7,
+      height: 8,
+      x: 6,
+      y: 6,
+      monsters: {
+        [`2:3`]: 1,
+        [`3:4`]: 1,
+      },
+    });
+  };
+
+  return (
+    <>
+      <button>{newId}</button>
+      <p>{singleRoom(newId).name}</p>
+      <p>
+        Name:
+        <input
+          type="text"
+          value={roomEdit.name}
+          onChange={(e) => setRoomEdit({ ...roomEdit, name: e.target.value })}
+        />
+      </p>
+      <p>
+        Width:
+        <input
+          type="number"
+          value={roomEdit.width}
+          onChange={(e) => setRoomEdit({ ...roomEdit, width: e.target.value })}
+        />
+      </p>
+      <p>
+        Height:
+        <input
+          type="number"
+          value={roomEdit.height}
+          onChange={(e) => setRoomEdit({ ...roomEdit, height: e.target.value })}
+        />
+      </p>
+      <p>
+        X:
+        <input
+          type="number"
+          value={roomEdit.x}
+          onChange={(e) => setRoomEdit({ ...roomEdit, x: e.target.value })}
+        />
+      </p>
+      <p>
+        Y:
+        <input
+          type="number"
+          value={roomEdit.y}
+          onChange={(e) => setRoomEdit({ ...roomEdit, y: e.target.value })}
+        />
+      </p>
+      <useMob test={roomEdit.monsters}>
+        <RoomMonsters
+          editMobs={editMobs}
+          roomEdit={roomEdit}
+          setRoomEdit={setRoomEdit}
+        />
+      </useMob>
+      <button onClick={() => submitRoom()}>Submit</button>
+      <button onClick={() => setIsEdit(false)}>Back</button>
+    </>
+  );
+};
+
+          value={roomEdit.x}
+          onChange={(e) => setRoomEdit({ ...roomEdit, x: e.target.value })}
+        />
+      </p>
+      <p>
+        Y:
+        <input
+          type="number"
+          value={roomEdit.y}
+          onChange={(e) => setRoomEdit({ ...roomEdit, y: e.target.value })}
+        />
+      </p>
+      <useMob test={roomEdit.monsters}>
+        <RoomMonsters
+          editMobs={editMobs}
+          roomEdit={roomEdit}
+          setRoomEdit={setRoomEdit}
+        />
+      </useMob>
+      <button onClick={() => submitRoom()}>Submit</button>
+      <button onClick={() => setIsEdit(false)}>Back</button>
+    </>
+  );
+};
+
     console.log("submit room");
     let temp = rooms.findIndex((room) => room.id === newId);
     temp >= 0 && (rooms[temp] = roomEdit);
