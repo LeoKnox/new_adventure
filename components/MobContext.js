684f3e41340971bc075d754ae08769214b9b79f3
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-05 16:17:43 -0700

    Update MobContext.js

--- components/MobContext.js
+++ components/MobContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-export const MobContext = createContext();
+export const MobCreateContext = createContext();
 
 export const useMob = () => {
   return useContext(MobContext);
