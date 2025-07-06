bc592f6430b2bf9d514e6394d351fb97b6f7114f
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-06 15:07:43 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -59,7 +59,7 @@ export default RoomMonsters = ({ room, editMobs, roomEdit, setRoomEdit, test }) 
           <input
             type="number"
             className="mobInput"
-            value={doors.x}
+            value={x}
             onChange={(e) => setNewMob({ ...newMob, x: e.target.value })}
           />
         </label>
@@ -68,7 +68,7 @@ export default RoomMonsters = ({ room, editMobs, roomEdit, setRoomEdit, test }) 
           <input
             type="number"
             className="mobInput"
-            value={doors.y}
+            value={y}
             onChange={(e) => setNewMob({ ...newMob, y: e.target.value })}
           />
         </label>
