490430835d63781a77b3da2b267a6f83b4c63015
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-11 13:54:09 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -31,7 +31,7 @@ export default RoomMonsters = ({
     //setRoomEdit(temp);
     temp = { ...temp, monsters: tempMobs };
     console.log(temp);
-    incDoors(`1,1`, 2);
+    incDoors("1,1", 2);
 
     console.log(roomEdit);
   };
