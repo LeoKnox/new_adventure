a5dd3502c973650364637e31fe5aeed37c57420a
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-12 16:45:38 -0700

    Create RoomContext.js

---
+++ components/RoomContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+export const MonstersContext = createContext();
+
+export const useMonstersContext = () => {
+  return useContext(MobCreateContext);
+};
+
+export const MonsterContextReturn = ({ children }) => {
+  console.log("mob context");
+  const [monstersList, setMonstersList] = useState([]);
+
+  const addMonsterContext = (newMonster) => {
+    console.log("add monster context");
+    setDoors([...monstersList, newMonster]);
+  };
+
+  return (
+    <MonstersContext.Provider value={{ doors, incDoors: increaseDoors }}>
+      {children}
+    </MonstersContext.Provider>
+  );
+};
