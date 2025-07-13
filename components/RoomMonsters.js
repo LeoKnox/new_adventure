53a88be4c2f1020d1fe5fa94e32c70a5627a6672
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-13 16:14:14 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -1,23 +1,103 @@
 import { createContext, useContext, useState } from "react";
+import { useMob } from "./MobContext.js";
+import { addMonsterContext } from "./RoomContext.js";
 
-export const MonstersContext = createContext();
-
-export const useMonstersContext = () => {
-  return useContext(MobCreateContext);
-};
-
-export const MonsterContextReturn = ({ children }) => {
-  console.log("monster context");
-  const [monstersList, setMonstersList] = useState([]);
-
-  const addMonsterContext = (newMonster) => {
-    console.log("add monster context");
-    setDoors([...monstersList, newMonster]);
+export default RoomMonsters = ({
+  room,
+  editMobs,
+  roomEdit,
+  setRoomEdit,
+  test,
+}) => {
+  console.log("room");
+  const [mobValues, setMobValues] = useState(Object.entries(roomEdit.monsters));
+  const [newMob, setNewMob] = useState({ x: 0, y: 0 });
+  const { doors, incDoors } = useMob();
+  console.table(mobValues);
+  const deleteMonster = (key) => {
+    let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    delete tempMobs[key];
+    temp.monsters = tempMobs;
+    setRoomEdit(temp);
   };
-
+  const addContextMob = () => {
+    console.log("add context mob");
+    let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    tempMobs = { [`1:1`]: 2 };
+    temp = { ...temp, monsters: tempMobs };
+    setRoomEdit(temp);
+  };
+  const addMob = (newMob) => {
+    console.log("add mob");
+    let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    tempMobs[`${newMob.y}"{newMob.x}`] = 1;
+    setRoomEdit(temp);
+  };
   return (
-    <MonstersContext.Provider value={{ monstersList, addMonsterContext }}>
-      {children}
-    </MonstersContext.Provider>
+    <>
+      <p>room monsters</p>
+      <button onClick={() => deleteMonster()}>update</button>
+      {Object.entries(mobValues).map(([key, value]) => (
+        <p>
+          <label>
+            Mob X
+            <input
+              type="number"
+              onChange={(e) => editMobs(key, value)}
+              value={value[0].split(":")[0]}
+            />
+          </label>
+          <label>
+            Mob Y
+            <input
+              type="number"
+              onChange={(e) =>
+                setMobValues((prevMob) => {
+                  const result = [...prevMob];
+                  result[key] = e.target.value;
+                  return result;
+                })
+              }
+              value={value[0].split(":")[1]}
+            />
+          </label>
+          <label>
+            Mob type
+            <input type="number" value={value[1]} />
+          </label>
+          <button onClick={() => deleteMonster(key)}>X</button>
+        </p>
+      ))}
+      {JSON.stringify(mobValues)}
+      <p>
+        <label>
+          New Mob X
+          <input
+            type="number"
+            className="mobInput"
+            value={doors[0]}
+            onChange={(e) => incDoors(e.target.value, 0)}
+          />
+        </label>
+        <label>
+          New Mob Y
+          <input
+            type="number"
+            className="mobInput"
+            value={doors[1]}
+            onChange={(e) => incDoors(e.target.value, 1)}
+          />
+        </label>
+        {doors}
+        <button onClick={addContextMob}>create mob</button>
+      </p>
+      <p>
+        {monstersList}
+        <button onClick={addMonsterContext}>red</button>
+      </p>
+    </>
   );
 };
