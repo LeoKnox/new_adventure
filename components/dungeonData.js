e69d75da7bdc173f4f8793310e8d5047b205768c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-14 21:09:11 -0700

    Update dungeonData.js

--- components/dungeonData.js
+++ components/dungeonData.js
@@ -27,48 +27,10 @@ let dungeonData = [
   },
 ];
 
-export const deleteMonsterDD = (monseterId) => {
+export const deleteMonsterDD = (monseterId, setRoomEdit) => {
   console.log("delete monster");
   delete dungeonData[1].monsters[`1:4`];
-  console.log(dungeonData);
-};
-
-export const allRooms = () => {
-  return dungeonData;
-};
-
-export const addRoom = (name, width, height, x, y) => {import { DemonSVG } from "./svgData";
-
-let dungeonData = [
-  {
-    id: 1,
-    name: "entry",
-    width: 9,
-    height: 5,
-    x: 5,
-    y: 5,
-    monsters: {
-      [`2:4`]: 1,
-      [`3:5`]: 1,
-    },
-  },
-  {
-    id: 2,
-    name: "entry",
-    width: 7,
-    height: 8,
-    x: 6,
-    y: 6,
-    monsters: {
-      [`1:4`]: 1,
-      [`1:5`]: 1,
-    },
-  },
-];
-
-export const deleteMonsterDD = (monseterId) => {
-  console.log("delete monster");
-  delete dungeonData[1].monsters[`1:4`];
+  setRoomEdit(dungeonData);
   console.log(dungeonData);
 };
 
@@ -77,29 +39,6 @@ export const allRooms = () => {
 };
 
 export const addRoom = (name, width, height, x, y) => {
-  newId = dungeonData.length
-    ? dungeonData[dungeonData.length - 1].id + 1
-    : (newId = 1);
-  dungeonData = [
-    ...dungeonData,
-    { id: newId, name: name, width: width, height: height, x: x, y: y },
-  ];
-  return dungeonData;
-};
-
-export const changeRoom = (newData = "chiiro") => {
-  dungeonData = dungeonData.map((room) => (room.id == 1 ? newData : room));
-  return dungeonData;
-};
-
-export const deleteRoom = (id = 1) => {
-  dungeonData = dungeonData.filter((room) => room.id != id);
-  return dungeonData;
-};
-export const singleRoom = (id = 1) => {
-  return dungeonData.find((room) => room.id == id);
-};
-
   newId = dungeonData.length
     ? dungeonData[dungeonData.length - 1].id + 1
     : (newId = 1);
