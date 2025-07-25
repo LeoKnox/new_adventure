f1cded75705280d4a0f174b3114f097cfe21847c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-07-25 14:15:45 -0700

    Update dungeonData.js

--- components/dungeonData.js
+++ components/dungeonData.js
@@ -42,11 +42,11 @@ export const addMonsterDD = (roomId, newMonsterID, newMonsterValue) => {
 export const editMonsterDD = (value, key, roomId) => {
   console.log("edit Monster");
   let newId = `${value[0].split(":")[0]}:${value[0].split(":")[1]}`;
-  console.log(dungeonData[roomId - 1]);
+  console.log(key + ":" + value);
   let temp = { ...dungeonData[1] };
   let tempMob = { ...temp.monsters };
   //console.log(dungeonData[roomId - 1].monsters[key]);
-  delete dungeonData[roomId - 1].monsters[key];
+  delete dungeonData[key].monsters[value];
   tempMob[newId] = value[1];
   dungeonData[1].monsters = tempMob;
 };
