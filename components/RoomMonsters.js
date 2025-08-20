b0437b32d8afc49e35b928004af8483963c5f949
LeoKnox <LeoKnox@users.noreply.github.com>
2025-08-20 13:58:20 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -71,7 +71,7 @@ export default RoomMonsters = ({
       console.log("type");
       console.log(tempMv);
       let x = [temp, value];
-      tempMv[value[0]][1] = x;
+      tempMv[0] = x;
 
       setMobValues(tempMv);
     }
