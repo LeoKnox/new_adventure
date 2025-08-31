0da14df427f160f042f2ea42945d7b524a966c71
LeoKnox <LeoKnox@users.noreply.github.com>
2025-08-30 17:44:59 -0700

    Update dungeonData.js

--- components/dungeonData.js
+++ components/dungeonData.js
@@ -1,80 +1,157 @@
-import { DemonSVG } from "./svgData";
+import { useState } from "react";
+import { useMob } from "./MobContext.js";
+import { addMonsterContext } from "./RoomContext.js";
+import {
+  deleteMonsterDD,
+  addMonsterDD,
+  editMonsterDD,
+  singleRoom,
+} from "./dungeonData.js";
 
-let dungeonData = [
-  {
-    id: 1,
-    name: "entry",
-    width: 9,
-    height: 5,
-    x: 5,
-    y: 5,
-    monsters: {
-      [`2:4`]: 1,
-      [`3:5`]: 1,
-    },
-  },
-  {
-    id: 2,
-    name: "entry",
-    width: 7,
-    height: 8,
-    x: 6,
-    y: 6,
-    monsters: {
-      [`1:4`]: 1,
-      [`1:5`]: 1,
-    },
-  },
-];
-
-export const deleteMonsterDD = (monsterId, roomId) => {
-  console.log("delete monster");
-  delete dungeonData[roomId - 1].monsters[monsterId];
-};
-
-export const addMonsterDD = (
+export default RoomMonsters = ({
   roomId,
-  newMonsterID,
-  newMonsterValue,
-  mobSelect
-) => {
-  console.log("add monster");
-  dungeonData[roomId - 1].monsters[`${newMonsterID[1]}:${newMonsterID[0]}`] =
-    mobSelect;
-};
-
-export const editMonsterDD = (mobValues) => {
-  console.log("edit Monster");
-  let temp = Object.fromEntries(mobValues);
-  console.log(temp);
-  dungeonData.monsters = temp;
-  console.log(dungeonData);
-};
-
-export const allRooms = () => {
-  return dungeonData;
-};
-
-export const addRoom = (name, width, height, x, y) => {
-  newId = dungeonData.length
-    ? dungeonData[dungeonData.length - 1].id + 1
-    : (newId = 1);
-  dungeonData = [
-    ...dungeonData,
-    { id: newId, name: name, width: width, height: height, x: x, y: y },
-  ];
-  return dungeonData;
-};
-
-export const changeRoom = (newData = "chiiro") => {
-  dungeonData = dungeonData.map((room) => (room.id == 1 ? newData : room));
-  return dungeonData;
-};
-
-export const deleteRoom = (id = 1) => {
-  dungeonData = dungeonData.filter((room) => room.id != id);
-  return dungeonData;
-};
-export const singleRoom = (id = 1) => {
-  return dungeonData.find((room) => room.id == id);
+  editMobs,
+  roomEdit,
+  setRoomEdit,
+  test,
+}) => {
+  console.log("room");
+  const [mobValues, setMobValues] = useState(Object.entries(roomEdit.monsters));
+  const [newMob, setNewMob] = useState({ x: 0, y: 0 });
+  const [mobSelect, setMobSelect] = useState(2);
+  const { doors, incDoors } = useMob();
+  console.log("room edit");
+  console.log(roomEdit);
+  const extDelete = (value) => {
+    {
+      deleteMonsterDD(value, roomId);
+    }
+    setMobValues(Object.entries(roomEdit.monsters));
+  };
+  const extAdd = (newMobValue, mobSelect) => {
+    console.log("ext add");
+    {
+      addMonsterDD(roomId, doors, newMobValue, mobSelect);
+    }
+    setMobValues(Object.entries(roomEdit.monsters));
+  };
+  const extEdit = () => {
+    console.log("ext edit");
+    editMonsterDD(mobValues);
+    setMobValues(Object.entries(roomEdit.monsters));
+  };
+  const deleteMonster = (key) => {
+    let temp = { ...roomEdit };
+    let tempMobs = { ...temp.monsters };
+    delete tempMobs[key];
+    temp.monsters = tempMobs;
+    setRoomEdit(temp);
+  };
+  const changeMob = (newVal, key, value, name) => {
+    console.log("change mob");
+    let tempMv = [...mobValues];
+    let temp = value[0].split(":");
+    if (name == "x") {
+      console.log("X");
+      temp[0] = newVal;
+      delete tempMv[key];
+      tempMv[key] = [`${temp[0]}:${temp[1]}`, value[1]];
+      setMobValues(tempMv);
+    }
+    if (name == "y") {
+      console.log("Y");
+      temp[1] = newVal;
+      tempMv[key] = [`${temp[0]}:${temp[1]}`, value[1]];
+      setMobValues(tempMv);
+    }
+    if (name == "type") {
+      console.log("type");
+      let x = [value[0], newVal];
+      tempMv[key] = x;
+      setMobValues(tempMv);
+    }
+    console.log({ [value[0]]: value[1] });
+  };
+  return (
+    <>
+      <p>room monsters</p>
+      <button onClick={() => deleteMonster()}>update</button>
+      {Object.entries(mobValues).map(([key, value]) => (
+        <p>
+          <label>
+            Mob X
+            <input
+              type="number"
+              name="x"
+              onChange={(e) =>
+                changeMob(e.target.value, key, value, e.target.name)
+              }
+              value={value[0].split(":")[0]}
+            />
+          </label>
+          <label>
+            Mob Y
+            <input
+              type="number"
+              name="y"
+              onChange={(e) =>
+                changeMob(e.target.value, key, value, e.target.name)
+              }
+              value={value[0].split(":")[1]}
+            />
+          </label>
+          <label>
+            Mob type
+            <input
+              type="number"
+              name="type"
+              onChange={(e) =>
+                changeMob((newVal = e.target.value), key, value, e.target.name)
+              }
+              value={value[1]}
+            />
+          </label>
+          <button onClick={() => extDelete(value[0])}>X</button>
+          <button id={roomEdit.monsters} onClick={extEdit}>
+            Update {value[0]} {key}
+          </button>
+        </p>
+      ))}
+      {JSON.stringify(mobValues)}
+      <p>
+        <label>
+          New Mob X
+          <input
+            type="number"
+            className="mobInput"
+            value={doors[0]}
+            onChange={(e) => incDoors(e.target.value, 0)}
+          />
+        </label>
+        <label>
+          New Mob Y
+          <input
+            type="number"
+            className="mobInput"
+            value={doors[1]}
+            onChange={(e) => incDoors(e.target.value, 1)}
+          />
+        </label>
+        <label>
+          New Mob
+          <select
+            id="mobSelect"
+            name="mobSelect"
+            value={mobSelect}
+            onChange={() => setMobSelect(event.target.value)}
+          >
+            <option value={`1`}>1</option>
+            <option value={`2`}>2</option>
+          </select>
+        </label>
+        {doors}
+        <button onClick={() => extAdd([`3:3`], mobSelect)}>create mob</button>
+      </p>
+    </>
+  );
 };
