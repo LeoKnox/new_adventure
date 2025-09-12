c4b0ca863a006da984564d76f59e3eaac5f3753c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-09-12 16:37:33 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -64,7 +64,7 @@ export default RoomMonsters = ({ roomId, roomEdit, setRoomEdit, test }) => {
     }
     //temp = tempMv;
     console.log(temp);
-    setRoomEdit(tempMv);
+    //setRoomEdit(tempMv);
     roomEdit.monsters = mobValues;
   };
   return (
