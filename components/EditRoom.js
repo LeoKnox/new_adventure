bba41ae06bc1bf81456013094c6886468acc7e64
LeoKnox <LeoKnox@users.noreply.github.com>
2025-09-13 17:38:02 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useCallback } from "react";
+import { useContext, useState, useCallback } from "react";
 import { MobContext } from "./MobContext.js";
 import { MonsterContextReturn } from "./RoomContext.js";
 import RoomMonsters from "./RoomMonsters.js";
@@ -13,87 +13,6 @@ export default EditRoom = ({
   removeMob,
 }) => {
   console.log("edit room");
-  console.table(MobContext);
-  const [roomEdit, setRoomEdit] = useState(singleRoom(newId));
-  const [testMob, setTestMob] = useState("mob text");
 
-  const submitRoom = (roomEdit) => {
-    console.log("submit room");
-    console.log(roomEdit);
-    changeRoomDD(roomEdit, newId);
-    setIsEdit(false);
-  };
-
-  const changeRoom = (name, value) => {
-    console.log("change room");
-    let temp = { ...roomEdit };
-    console.log(temp);
-    temp[name] = value;
-    setRoomEdit(temp);
-    //changeRoomDD(temp, newId);
-    setRooms(allRooms());
-  };
-
-  return (
-    <>
-      <button>{newId}</button>
-      <p>{singleRoom(newId).name}</p>
-      <p>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={roomEdit.name}
-          onChange={(e) => changeRoom(e.target.name, e.target.value)}
-        />
-      </p>
-      <p>
-        Width:
-        <input
-          type="number"
-          name="width"
-          value={roomEdit.width}
-          onChange={(e) => changeRoom(e.target.name, e.target.value)}
-        />
-      </p>
-      <p>
-        Height:
-        <input
-          type="number"
-          name="height"
-          value={roomEdit.height}
-          onChange={(e) => changeRoom(e.target.name, e.target.value)}
-        />
-      </p>
-      <p>
-        X:
-        <input
-          type="number"
-          name="x"
-          value={roomEdit.x}
-          onChange={(e) => changeRoom(e.target.name, e.target.value)}
-        />
-      </p>
-      <p>
-        Y:
-        <input
-          type="number"
-          name="y"
-          value={roomEdit.y}
-          onChange={(e) => changeRoom(e.target.name, e.target.value)}
-        />
-      </p>
-      <MonsterContextReturn monstersList={["green"]}>
-        <MobContext test={roomEdit.monsters}>
-          <RoomMonsters
-            roomEdit={roomEdit}
-            setRoomEdit={setRoomEdit}
-            roomId={newId}
-          />
-        </MobContext>
-      </MonsterContextReturn>
-      <button onClick={() => submitRoom(roomEdit)}>Submit</button>
-      <button onClick={() => setIsEdit(false)}>Back</button>
-    </>
-  );
+  return <></>;
 };
