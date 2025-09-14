0e1ac8ac0e8be8f14a5f2511111f37649476ca70
LeoKnox <LeoKnox@users.noreply.github.com>
2025-09-13 18:02:25 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -15,12 +15,34 @@ export default EditRoom = ({
   console.log("edit room");
   console.log(rooms);
   const [currentRoom, setCurrentRoom] = useState(singleRoom(newId));
-  const { id, name, x, y, length, width, monsters } = currentRoom;
+  const { id, name, x, y, height, width, monsters } = currentRoom;
   return (
     <>
-      <label>
-        Name: <input type="text" value={name} />
-      </label>
+      <p>
+        <label>
+          Name: <input type="text" value={name} />
+        </label>
+      </p>
+      <p>
+        <label>
+          X: <input type="text" value={x} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Y: <input type="text" value={y} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Width: <input type="text" value={width} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Height: <input type="text" value={height} />
+        </label>
+      </p>
       <button onClick={() => setCurrentRoom({ name: "red" })}>update</button>
     </>
   );
