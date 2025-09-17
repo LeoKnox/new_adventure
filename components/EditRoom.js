6f8eb5c637d451992a9b5976b90e63986a00c4a3
LeoKnox <LeoKnox@users.noreply.github.com>
2025-09-16 20:10:43 -0700

    Update EditRoom.js

--- components/EditRoom.js
+++ components/EditRoom.js
@@ -4,15 +4,7 @@ import { MonsterContextReturn } from "./RoomContext.js";
 import RoomMonsters from "./RoomMonsters.js";
 import { singleRoom, changeRoomDD, allRooms } from "./dungeonData.js";
 
-export default EditRoom = ({
-  newId = 1,
-  setIsEdit,
-  rooms,
-  setRooms,
-  editFunc,
-  submitRoom,
-  removeMob,
-}) => {
+export default EditRoom = ({ newId = 1, setIsEdit, editFunc, submitRoom }) => {
   console.log("edit room");
   const [currentRoom, setCurrentRoom] = useState(singleRoom(newId));
   const { id, name, x, y, height, width, monsters } = currentRoom;
