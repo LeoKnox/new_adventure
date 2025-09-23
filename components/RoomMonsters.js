ef65f17355c96bea841eb5beb0c28565df5f67f1
LeoKnox <LeoKnox@users.noreply.github.com>
2025-09-22 17:18:29 -0700

    Update RoomMonsters.js

--- components/RoomMonsters.js
+++ components/RoomMonsters.js
@@ -15,7 +15,7 @@ export default RoomMonsters = ({ monsters, mId, changeMob }) => {
   const createMonster = (e) => {
     console.log("create room");
     e.preventDefault();
-    const formData = e.elements.x;
+    const formData = new FormData(e.elements);
     console.log(formData);
   };
   return (
