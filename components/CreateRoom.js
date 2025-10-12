ebd29e2f88be7b131fda229ad70568af92a2cd86
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-11 18:00:15 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -5,7 +5,7 @@ export default CreateRoom = () => {
   return (
     <>
       <h1>create room map</h1>
-      <table>{(Array.from({ length: 2 }), () => <p>i</p>)}</table>
+      <table>{(Array.from({ length: 2 }), () => <p>"i"</p>)}</table>
     </>
   );
 };
