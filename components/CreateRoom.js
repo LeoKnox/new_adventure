4473be769d42e13ba85d9be93ea1bfb9606efda6
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-12 16:23:24 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -1,15 +1,15 @@
 export default CreateRoom = () => {
   const height = 2;
   const width = 2;
-  const tempList = Array.from({ length: 2 }, (v) => {
-    <p>"v"</p>;
+  const tempList = Array.from({ length: 2 }, (i) => {
+    return <p>v + {i}</p>;
   });
   console.log("create room new");
   console.log(tempList);
   return (
     <>
       <h1>create room map</h1>
-      {tempList}
+      {[...tempList]}
     </>
   );
 };
