e1976b1bcd6663e069de01ef9402a7cea5c1876e
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-13 16:53:06 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -4,19 +4,17 @@ export default CreateRoom = () => {
   const [height, setHeight] = useState(2);
   const width = 2;
   const arrow = <button onClick={() => setHeight(height + 1)}>></button>;
-  const [tempList, setTempList] = useState(
-    Array.from({ length: 2 }, (v, i) => {
-      const tempRow = Array.from({ length: 3 }, (w, j) => {
-        return <td>i</td>;
-      });
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    })
-  );
+  const tempList = Array.from({ length: 2 }, (v, i) => {
+    const tempRow = Array.from({ length: 3 }, (w, j) => {
+      return <td>i</td>;
+    });
+    return (
+      <tr>
+        {tempRow}
+        <td>{arrow}</td>
+      </tr>
+    );
+  });
   console.log("create room new");
   console.log(tempList);
   return (
