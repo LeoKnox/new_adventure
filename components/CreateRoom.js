6f52e97d1e9dd634bc8a52b70f9d3d25da4b40b0
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-13 16:29:06 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -3,7 +3,7 @@ export default CreateRoom = () => {
   const width = 2;
   const arrow = <button>></button>;
   const tempList = Array.from({ length: 2 }, (v, i) => {
-    const tempRow = Array.from({ length: 3 }, () => {
+    const tempRow = Array.from({ length: 3 }, (w, j) => {
       return <td>i</td>;
     });
     return (
