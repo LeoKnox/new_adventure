c8dff438f2b4947745a4199dfb56f960a3fd4524
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-14 13:43:58 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -2,21 +2,23 @@ import { useState, useEffect } from "react";
 
 export default CreateRoom = () => {
   const [height, setHeight] = useState(2);
-  const [tempList, setTempList] = []
+  const [tempList, setTempList] = [];
   const width = 2;
   const arrow = <button onClick={() => resetRoom()}>></button>;
   useEffect(() => {
-    setTempList(Array.from({ length: 2 }, (v, i) => {
-      const tempRow = Array.from({ length: 3 }, (w, j) => {
-        return <td>i</td>;
-      }));
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    });
+    setTempList(
+      Array.from({ length: 2 }, (v, i) => {
+        const tempRow = Array.from({ length: 3 }, (w, j) => {
+          return <td>i</td>;
+        });
+        return (
+          <tr>
+            {tempRow}
+            <td>{arrow}</td>
+          </tr>
+        );
+      })
+    );
     const resetRoom = () => {
       console.log("reset room");
       setHeight(height + 1);
