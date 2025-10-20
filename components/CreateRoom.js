7d625c6b8008dd01c107c0f7afc05cd643f7755a
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-20 10:51:15 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -4,17 +4,7 @@ export default CreateRoom = () => {
   const [height, setHeight] = useState(2);
   const [width, setWidth] = useState(3);
   const arrow = <button onClick={() => setWidth(width + 1)}>→</button>;
-  const downArrow = <button onClick={() => setWidth(height + 1)}>↓</button>;
-
-  const [tempList, setTempList] = useState(
-    Array.from({ length: height }, (v, i) => {
-      const tempRow = Array.from({ length: width }, (w, j) => {import { useState, useEffect } from "react";
-
-export default CreateRoom = () => {
-  const [height, setHeight] = useState(2);
-  const [width, setWidth] = useState(3);
-  const arrow = <button onClick={() => setWidth(width + 1)}>→</button>;
-  const downArrow = <button onClick={() => setWidth(height + 1)}>↓</button>;
+  const downArrow = <button onClick={() => setHeight(height + 1)}>↓</button>;
 
   const [tempList, setTempList] = useState(
     Array.from({ length: height }, (v, i) => {
@@ -48,68 +38,6 @@ export default CreateRoom = () => {
     });
     temp.push(<tr>{downArrow}</tr>);
     setTempList(temp);
-  }, [height, width]);
-
-  /*const resetRoom = () => {
-    console.log("reset room");
-    console.log(width);
-    let tempWidth = width + 1;
-    setWidth(tempWidth);
-    let temp = Array.from({ length: height }, (v, i) => {
-      const tempRow = Array.from({ length: width + 1 }, (w, j) => {
-        return <td>+i+</td>;
-      });
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    });
-
-    console.log("contine reset");
-    console.log(width);
-    setTempList(temp);
-  };*/
-  console.log("create room new");
-  console.log(tempList);
-  return (
-    <>
-      <h1>create room map {width}</h1>
-      {tempList}
-    </>
-  );
-};
-
-        return <td>-i-</td>;
-      });
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    })
-  );
-
-  useEffect(() => {
-    let temp = Array.from({ length: height }, (v, i) => {
-      const tempRow = Array.from({ length: width }, (w, j) => {
-        return (
-          <>
-            <td>i</td>
-          </>
-        );
-      });
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    });
-    setTempList(temp);
-    <td>{downArrow}</td>;
   }, [height, width]);
 
   /*const resetRoom = () => {
