115a8cdb4aa469f408132e92f0a84cf4f650e4d2
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-23 17:43:53 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -1,90 +1,89 @@
 import { useState, useEffect } from "react";
-import { Level } from "./Level.js";
+import { allRooms, addRoom, changeRoom, singleRoom } from "./dungeonData.js";
+import AllRooms from "./AllRooms.js";
+import { MonsterContextReturn } from "./RoomContext.js";
+import { deleteRoom } from "./dungeonData.js";
+import NewRoom from "./NewRoom.js";
+import EditRoom from "./EditRoom.js";
+import CreateRoom from "./CreateRoom.js";
 
-export default CreateRoom = () => {
-  const [height, setHeight] = useState(2);
-  const [width, setWidth] = useState(3);
-  const [show, setShow] = useState(false);
-  const [roomDrag, setRoomDrag] = useState(false);
-  const arrow = <button onClick={() => setWidth(width + 1)}>→</button>;
-  const downArrow = <button onClick={() => setHeight(height + 1)}>↓</button>;
+export default Build = () => {
+  console.log("build.js");
 
-  const [tempList, setTempList] = useState();
+  const [isEdit, setIsEdit] = useState(false);
+  const [loadComponent, setLoadComponent] = useState("b");
+  const [newId, setNewId] = useState(1);
+  const [rooms, setRooms] = useState(allRooms());
 
-  const addItem = (e) => {
-    console.log("add item");
-    console.log(e.target.value[0]);
-    setShow(true);
+  const editFunc = (roomEdit, newMob) => {
+    console.log("edit func");
+    console.log(newMob);
+    roomEdit.monsters[`${newMob.x}:${newMob.y}`] = 1;
+    setRooms(roomEdit);
   };
-  const onDown = () => {
-    console.log("red");
-    onMove();
-    //setShow(false);
+  const loadEdit = (roomId = 1, editValue) => {
+    let temp = singleRoom(roomId);
+    setNewId(roomId);
+    setLoadComponent(editValue);
+    //setIsEdit(!isEdit);
   };
-  const onMove = () => {
-    console.log("move");
-  };
-  const dropDown = () => {
-    return (
-      <>
-        <select>
-          <option>monster</option>
-        </select>
-      </>
+  const loadMap = (roomId = 1) => {
+    console.log("load map");
+    setIsEdit(
+      <EditRoom newId={newId} setIsEdit={setIsEdit} submitRoom={submitRoom} />
     );
   };
-
-  useEffect(() => {
-    let temp = Array.from({ length: height }, (v, i) => {
-      const tempRow = Array.from({ length: width }, (w, j) => {
-        return (
-          <>
-            <td>
-              <button value={[j, i]} onClick={addItem} onMouseDown={onDown}>
-                {show ? dropDown() : "j"}
-              </button>
-            </td>
-          </>
-        );
-      });
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    });
-    temp.push(<tr>{downArrow}</tr>);
-    setTempList(temp);
-  }, [height, width, show]);
-
-  /*const resetRoom = () => {
-    console.log("reset room");
-    console.log(width);
-    let tempWidth = width + 1;
-    setWidth(tempWidth);
-    let temp = Array.from({ length: height }, (v, i) => {
-      const tempRow = Array.from({ length: width + 1 }, (w, j) => {
-        return <td>+i+</td>;
-      });
-      return (
-        <tr>
-          {tempRow}
-          <td>{arrow}</td>
-        </tr>
-      );
-    });
-
-    console.log("contine reset");
-    console.log(width);
-    setTempList(temp);
-  };*/
-  console.log("create room new");
-  console.log(tempList);
+  const removeRoom = (deleteId) => {
+    console.log("remove room");
+    console.log(deleteId);
+    deleteRoom(deleteId);
+    setRooms(allRooms());
+  };
+  const submitRoom = (newRoom) => {
+    console.log("submit rooms");
+    let temp = allRooms();
+  };
+  const removeMob = (key) => {
+    console.log("remove mob");
+    let temp = rooms[newId];
+    let temptwo = temp.monsters;
+    delete temptwo[key];
+    setRooms(temp);
+  };
+  const tempPage = (
+    <EditRoom newId={newId} setIsEdit={setIsEdit} submitRoom={submitRoom} />
+  );
+  const roomsObj = {
+    a: (
+      <EditRoom
+        newId={newId}
+        setLoadComponent={setLoadComponent}
+        submitRoom={submitRoom}
+      />
+    ),
+    b: (
+      <>
+        <AllRooms
+          rooms={rooms}
+          removeRoom={removeRoom}
+          loadEdit={loadEdit}
+          loadMap={loadMap}
+        />
+        <NewRoom
+          rooms={rooms}
+          setRooms={setRooms}
+          submitRoom={submitRoom}
+          rooms={rooms}
+        />
+      </>
+    ),
+    map: <CreateRoom />,
+  };
+  //<EditRoom newId={newId} setIsEdit={setIsEdit} submitRoom={submitRoom} />
   return (
     <>
-      <h1>create room map {width}</h1>
-      {tempList}
+      <p>build a dungeon</p>
+      {roomsObj[loadComponent]}
     </>
   );
 };
