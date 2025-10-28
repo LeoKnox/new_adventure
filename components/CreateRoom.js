31aee10a216b539f86201d3402566b88691c5662
LeoKnox <LeoKnox@users.noreply.github.com>
2025-10-27 18:13:46 -0700

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -1,95 +1,107 @@
 import { useState, useEffect } from "react";
-import { allRooms, addRoom, changeRoom, singleRoom } from "./dungeonData.js";
-import AllRooms from "./AllRooms.js";
-import { MonsterContextReturn } from "./RoomContext.js";
-import { deleteRoom } from "./dungeonData.js";
-import NewRoom from "./NewRoom.js";
-import EditRoom from "./EditRoom.js";
-import CreateRoom from "./CreateRoom.js";
+import { singleRoom } from "./dungeonData.js";
+import { Level } from "./Level.js";
 
-export default Build = () => {
-  console.log("build.js");
+export default CreateRoom = () => {
+  const [height, setHeight] = useState(singleRoom().height);
+  const [width, setWidth] = useState(singleRoom().width);
+  const [drag, setDrag] = useState(false);
+  const [show, setShow] = useState(false);
+  const [roomDrag, setRoomDrag] = useState(false);
+  const arrow = <button onClick={() => setWidth(width + 1)}>→</button>;
+  const downArrow = (
+    <button onClick={() => setHeight(height + 1)} style={{ width: width * 40 }}>
+      ↓
+    </button>
+  );
 
-  const [isEdit, setIsEdit] = useState(false);
-  const [loadComponent, setLoadComponent] = useState("b");
-  const [newId, setNewId] = useState(1);
-  const [rooms, setRooms] = useState(allRooms());
+  const [tempList, setTempList] = useState();
 
-  const editFunc = (roomEdit, newMob) => {
-    console.log("edit func");
-    console.log(newMob);
-    roomEdit.monsters[`${newMob.x}:${newMob.y}`] = 1;
-    setRooms(roomEdit);
+  const addItem = (e) => {
+    console.log("add item");
+    console.log(e.target.value[0]);
+    setShow(false);
   };
-  const loadEdit = (roomId = 1, editValue) => {
-    let temp = singleRoom(roomId);
-    setNewId(roomId);
-    setLoadComponent(editValue);
-    //setIsEdit(!isEdit);
+  const onDown = (e) => {
+    console.log("red");
+    setDrag(true);
+    //setShow(false);
   };
-  const loadMap = (roomId = 1) => {
-    console.log("load map");
-    setIsEdit(
-      <EditRoom newId={newId} setIsEdit={setIsEdit} submitRoom={submitRoom} />
+  const onMove = (e) => {
+    if (setDrag) {
+      console.log("move" + e.clientX);
+    }
+  };
+  const onUp = (e) => {
+    setDrag(false);
+  };
+  const dropDown = () => {
+    return (
+      <>
+        <select>
+          <option>monster</option>
+        </select>
+      </>
     );
   };
-  const removeRoom = (deleteId) => {
-    console.log("remove room");
-    console.log(deleteId);
-    deleteRoom(deleteId);
-    setRooms(allRooms());
-  };
-  const submitRoom = (newRoom) => {
-    console.log("submit rooms");
-    let temp = allRooms();
-  };
-  const removeMob = (key) => {
-    console.log("remove mob");
-    let temp = rooms[newId];
-    let temptwo = temp.monsters;
-    delete temptwo[key];
-    setRooms(temp);
-  };
-  const tempPage = (
-    <EditRoom newId={newId} setIsEdit={setIsEdit} submitRoom={submitRoom} />
-  );
-  const roomsObj = {
-    a: (
-      <EditRoom
-        newId={newId}
-        setLoadComponent={setLoadComponent}
-        submitRoom={submitRoom}
-      />
-    ),
-    b: (
-      <>
-        <AllRooms
-          rooms={rooms}
-          removeRoom={removeRoom}
-          loadEdit={loadEdit}
-          loadMap={loadMap}
-        />
-        <NewRoom
-          rooms={rooms}
-          setRooms={setRooms}
-          submitRoom={submitRoom}
-          rooms={rooms}
-        />
-      </>
-    ),
-    map: (
-      <CreateRoom
-        newId={newId}
-        setLoadComponent={setLoadComponent}
-        submitRoom={submitRoom}
-      />
-    ),
-  };
-  //<EditRoom newId={newId} setIsEdit={setIsEdit} submitRoom={submitRoom} />
+
+  useEffect(() => {
+    let temp = Array.from({ length: height }, (v, i) => {
+      const tempRow = Array.from({ length: width }, (w, j) => {
+        return (
+          <>
+            <td style={{ width: "40px" }}>
+              <button
+                value={[j, i]}
+                //onClick={addItem}
+                onMouseMove={onMove}
+                onMouseDown={onDown}
+                onMouseup={onUp}
+              >
+                {show ? dropDown() : "j"}
+              </button>
+            </td>
+          </>
+        );
+      });
+      return (
+        <tr>
+          {tempRow}
+          <td>{arrow}</td>
+        </tr>
+      );
+    });
+    temp.push(<>{downArrow}</>);
+    setTempList(temp);
+  }, [height, width, show]);
+
+  /*const resetRoom = () => {
+    console.log("reset room");
+    console.log(width);
+    let tempWidth = width + 1;
+    setWidth(tempWidth);
+    let temp = Array.from({ length: height }, (v, i) => {
+      const tempRow = Array.from({ length: width + 1 }, (w, j) => {
+        return <td>+i+</td>;
+      });
+      return (
+        <tr>
+          {tempRow}
+          <td>{arrow}</td>
+        </tr>
+      );
+    });
+
+    console.log("contine reset");
+    console.log(width);
+    setTempList(temp);
+  };*/
+  console.log("create room new");
+  console.log(tempList);
   return (
     <>
-      <p>build a dungeon</p>
-      {roomsObj[loadComponent]}
+      <h1>create room map {width}</h1>
+      {tempList}
     </>
   );
 };
