7315b82043ed9c46cbff39c4e9ead1393f6709bf
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-07 15:35:36 -0800

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -104,6 +104,132 @@ export default CreateRoom = ({ newId, setLoadComponent, submitRoom }) => {
     <>
       <div>
         <h1>create room map {width}</h1>
+        <div style={{ gridTemplateColumns: "1fr/1fr/1fr" }}>
+          <div style={{ gridColumn: "1/4" }}>{upArrow}</div>
+          <div
+            style={{
+              position: "relative",
+              top: "0",
+              left: "0",
+              width: "80%",
+              height: "80%",
+              display: "inline-block",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: "0",
+                left: "0",
+                width: "80%",
+                height: "80%",
+              }}
+            >
+              {tempList}
+            </div>
+
+            <div
+              style={{
+                position: "absolute",
+                top: "0",
+                left: "0",
+                width: "80%",
+                height: "80%",
+              }}
+            >
+              <tr>
+                <td style={{ width: "40px" }}>*</td>
+                <td style={{ width: "40px" }}>*</td>
+                <td style={{ width: "40px" }}>*</td>
+              </tr>
+              <tr>
+                <td>*</td>
+                <td>*</td>
+                <td>*</td>
+              </tr>
+              <tr>
+                <td>*</td>
+                <td>*</td>
+                <td>*</td>
+              </tr>
+            </div>
+          </div>
+          <div style={{ position: "static" }}>{downArrow}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+  };
+  const onMove = (e) => {
+    if (setDrag) {
+      console.log("move" + e.clientX);
+    }
+  };
+  const onUp = (e) => {
+    setDrag(false);
+  };
+  const dropDown = () => {
+    return (
+      <>
+        <select>
+          <option>monster</option>
+        </select>
+      </>
+    );
+  };
+
+  useEffect(() => {
+    let temp = Array.from({ length: height }, (v, i) => {
+      const tempRow = Array.from({ length: width }, (w, j) => {
+        return (
+          <td style={{ width: "40px" }}>
+            <button
+              value={[j, i]}
+              //onClick={addItem}
+              //onMouseMove={onMove}
+              //onMouseDown={onDown}
+              //onMouseup={onUp}
+            >
+              {show ? dropDown() : "j"}
+            </button>
+          </td>
+        );
+      });
+      return <tr>{tempRow}</tr>;
+    });
+    //temp.unshift(<p>{upArrow}</p>);
+
+    setTempList(temp);
+  }, [height, width, show]);
+
+  /*const resetRoom = () => {
+    console.log("reset room");
+    console.log(width);
+    let tempWidth = width + 1;
+    setWidth(tempWidth);
+    let temp = Array.from({ length: height }, (v, i) => {
+      const tempRow = Array.from({ length: width + 1 }, (w, j) => {
+        return <td>+i+</td>;
+      });
+      return (
+        <tr>
+          {tempRow}
+          <td>{arrow}</td>
+        </tr>
+      );
+    });
+
+    console.log("contine reset");
+    console.log(width);
+    setTempList(temp);
+  };*/
+  console.log("create room new");
+  console.log(tempList);
+  return (
+    <>
+      <div>
+        <h1>create room map {width}</h1>
         <div style={{ position: "relative" }}>
           {upArrow}
           <div
