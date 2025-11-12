7709d04e286588255166c6f304e6aee25f2b6ff3
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-12 11:32:12 -0800

    Update CreateRoom.js

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -112,7 +112,8 @@ export default CreateRoom = ({ newId, setLoadComponent, submitRoom }) => {
             //position: "absolute",
           }}
         >
-          <div style={{ gridcolumn: "3/4", gridRow: "1/2" }}>{upArrow}</div>
+          <div style={{ gridColumn: "1/2", gridRow: "1/2" }}>*</div>
+          <div style={{ gridcolumn: "2/3", gridRow: "1/2" }}>{upArrow}</div>
           <div style={{ gridColumn: "1/2", gridRow: "2/3" }}>{leftarrow}</div>
           <div style={{ gridColumn: "3/4", gridRow: "2/3" }}>{arrow}</div>
           <div style={{ gridColumn: "2/3", gridRow: "2/3" }}>{tempList}</div>
