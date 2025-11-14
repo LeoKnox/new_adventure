9252eed6ed7a909a96d1ee3ee721ca366b98359c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-14 14:29:46 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -6,14 +6,6 @@ let characterData = [
     lvl: 1,
     atk: 10,
     def: 10,
-    weapon: ["spear"],let characterData = [
-  {
-    id: 1,
-    name: "ao",
-    icon: 0,
-    lvl: 1,
-    atk: 10,
-    def: 10,
     weapon: ["spear"],
     armor: [],
   },
@@ -59,66 +51,8 @@ export const addWeapon = (awId = 0, newWeapon = "Seax") => {
   console.log("add weapon");
   let temp = { ...characterData[1].weapons, newWeapon };
   console.log(awId);
+  console.log("+");
   characterData[1].weapon = temp;
-};
-
-export const addInventory = (
-  characterId = 1,
-  type = "armor",
-  item = "scale"
-) => {
-  let temp = characterData.find((character) => character.id == characterId);
-  temp = { ...temp, [type]: [...temp[type], item] };
-  characterData = characterData.map((character) =>
-    character.id == characterId ? temp : character
-  );
-};
-
-    armor: [],
-  },
-  {
-    id: 2,
-    name: "aka",
-    icon: 0,
-    lvl: 1,
-    atk: 12,
-    def: 9,
-    weapon: ["sword"],
-    armor: [],
-  },
-];
-
-export const addCharacter = (newCharacter) => {
-  newCharacter.weapon = [];
-  newCharacter.armor = [];
-  if (characterData.length == 0) {
-    characterData = newCharacter;
-  } else {
-    newCharacter.id = characterData[characterData.length - 1].id + 1;
-    let temp = [...characterData, newCharacter];
-    characterData = temp;
-  }
-};
-
-export const allCharacters = () => {
-  return characterData;
-};
-
-export const deleteCharacter = (deleteId) => {
-  let temp = characterData.filter((character) => character.id != deleteId);
-  characterData = temp;
-};
-
-export const singleCharacter = (id = 1) => {
-  let temp = characterData.find((character) => character.id == id);
-  return temp;
-};
-
-export const addWeapon = (id = 0, newWeapon = "Seax") => {
-  console.log("add weapon");
-  let temp = { ...characterData[1].weapons, newWeapon };
-  console.log(characterData[0].weapon);
-  characterData[+id].weapon = temp;
 };
 
 export const addInventory = (
