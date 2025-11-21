562af87f74f74abe4f47609e2d0ddbba96cfbf5d
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-20 16:00:41 -0800

    Update View.js

--- components/View.js
+++ components/View.js
@@ -1,47 +1,29 @@
-import { useState, useEffect } from "react";
-import { singleCharacter, addWeapon, addInventory } from "./characterData.js";
-import AddArmor from "./AddArmor.js";
-import AddWeapon from "./AddWeapon.js";
-import DisplayArmor from "./DisplayArmor.js";
-import DisplayWeapon from "./DisplayWeapon.js";
+import { useState } from "react";
 
-export default View = ({ characterId }) => {
-  const [character, setCharacter] = useState({});
-  const [weaponToAdd, setWeaponToAdd] = useState("kama");
-  const updateInventory = (type = "armor", item = "leather") => {
-    addInventory(characterId, type, item);
-
-    setCharacter(singleCharacter(characterId));
+export default AddWeapon = ({
+  updateInventory,
+  setWeaponToAdd,
+  characterWeapons,
+}) => {
+  const [listWeapon, setListWeapon] = useState("kama");
+  availableWeapons = ["saber", "cutless"];
+  // will set state to first on weapon list
+  const addNewWeapon = () => {
+    updateInventory("weapon", listWeapon);
   };
-  const viewAddWeapon = () => {
-    addWeapon(characterId - 1, weaponToAdd);
-    setCharacter({ ...singleCharacter(characterId) });
-  };
-
-  useEffect(() => {
-    console.log("view use effect");
-    let temp = {};
-    temp = singleCharacter(characterId);
-    setCharacter(temp);
-  }, []);
-
   return (
-    <>
-      <h3>Character</h3>
-      <h3>id: {characterId}</h3>
-      <p>Name: {character.name}</p>
-      <p>Icon: {character.icon}</p>
-      <p>{character.weapon}</p>
-      <p>Lvl: {character.lvl}</p>
-      <p>Atk: {character.atk}</p>
-      <p>Def: {character.def}</p>
-      <AddWeapon
-        updateInventory={viewAddWeapon}
-        setWeaponToAdd={setWeaponToAdd}
-      />
-      <DisplayWeapon characterWeapon={character.weapon} />
-      <AddArmor updateInventory={updateInventory} />
-      <DisplayArmor characterArmor={character.armor} />
-    </>
+    <div>
+      <p>Add {listWeapon}</p>
+      <br />
+      <select onChange={(e) => setWeaponToAdd(e.target.value)}>
+        <option>kama</option>
+        <option>sai</option>
+        {availableWeapons.map((i) => (
+          <option>{i}</option>
+        ))}
+      </select>
+      <br />
+      <button onClick={addNewWeapon}>Add Weapon</button>
+    </div>
   );
 };
