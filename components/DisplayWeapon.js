567bcfa06dbe158c26af36f1c82c80912b3a893b
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-23 16:40:03 -0800

    Update DisplayWeapon.js

--- components/DisplayWeapon.js
+++ components/DisplayWeapon.js
@@ -1,24 +1,72 @@
-import { deleteWeapon } from "./characterData.js";
+let characterData = [
+  {
+    id: 1,
+    name: "ao",
+    icon: 0,
+    lvl: 1,
+    atk: 10,
+    def: 10,
+    weapon: ["spear"],
+    armor: [],
+  },
+  {
+    id: 2,
+    name: "aka",
+    icon: 0,
+    lvl: 1,
+    atk: 12,
+    def: 9,
+    weapon: ["sword"],
+    armor: [],
+  },
+];
 
-export default DisplayWeapon = ({ characterWeapon }) => {
-  const deleteWeaponPage = (id) => {
-    console.log("delete weapon");
-    //console.log(e.target.id);
-    //let temp = e.target.id;
-    deleteWeapon(id);
-  };
-  return (
-    <div>
-      <ul>
-        {characterWeapon?.map((weapon, id) => (
-          <li>
-            {weapon}
-            <button id={id} onClick={(e) => deleteWeaponPage(e.target.id)}>
-              delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+export const addCharacter = (newCharacter) => {
+  newCharacter.weapon = [];
+  newCharacter.armor = [];
+  if (characterData.length == 0) {
+    characterData = newCharacter;
+  } else {
+    newCharacter.id = characterData[characterData.length - 1].id + 1;
+    let temp = [...characterData, newCharacter];
+    characterData = temp;
+  }
+};
+
+export const allCharacters = () => {
+  return characterData;
+};
+
+export const deleteCharacter = (deleteId) => {
+  let temp = characterData.filter((character) => character.id != deleteId);
+  characterData = temp;
+};
+
+export const singleCharacter = (id = 1) => {
+  let temp = characterData.find((character) => character.id == id);
+  return temp;
+};
+
+export const addWeapon = (awId = 0, newWeapon = "Seax") => {
+  console.log("add weapon");
+  let temp = characterData[awId].weapon;
+  temp.push(newWeapon);
+  characterData[awId].weapon = temp;
+};
+
+export const addInventory = (
+  characterId = 1,
+  type = "armor",
+  item = "scale"
+) => {
+  let temp = characterData.find((character) => character.id == characterId);
+  temp = { ...temp, [type]: [...temp[type], item] };
+  characterData = characterData.map((character) =>
+    character.id == characterId ? temp : character
   );
+};
+
+export const deleteWeapon = (delId) => {
+  console.log("delete weapon");
+  console.log(delId);
 };
