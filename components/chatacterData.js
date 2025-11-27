51084baadcff09d4e9b8f46bf4c76dc91e21eb5c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-27 14:06:27 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -76,5 +76,5 @@ export const deleteWeapon = (delId, charId = 1) => {
 export const deleteItem = (delId, charId = 1, reference) => {
   console.log("delete Item");
   console.log(delId + ":" + charId + ":" + reference);
-  //delete characterData[charId - 1].weapon[delId];
+  delete characterData[charId].weapon[delId];
 };
