411b6d95a949a2850da66c1f5297f6ac24fc7d70
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-28 19:25:12 -0800

    Update DisplayArmor.js

--- components/DisplayArmor.js
+++ components/DisplayArmor.js
@@ -4,7 +4,7 @@ export default DisplayArmor = ({ characterArmor, delArmor }) => {
       <ul>
         {characterArmor?.map((armor, armorId) => (
           <li>
-            <input type="radio" id="armor" name="currentArmor" value={armor} />
+            <input type="radio" id="armor" name="currentArmor" value="test" />
             {armor}
             <button onClick={() => delArmor(armorId)}>delete</button>
           </li>
