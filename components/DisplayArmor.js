322c368ee7cb8cb18185b9ef5463e1b79a7ef65d
LeoKnox <LeoKnox@users.noreply.github.com>
2025-11-30 18:12:24 -0800

    Update DisplayArmor.js

--- components/DisplayArmor.js
+++ components/DisplayArmor.js
@@ -1,10 +1,13 @@
 export default DisplayArmor = ({ characterArmor, delArmor }) => {
+  let selectedArmor = 1;
   return (
     <>
       <ul>
         {characterArmor?.map((armor, armorId) => (
           <div>
-            <button style={{ background: armorId == 1 ? "blue" : "red" }}>
+            <button
+              style={{ background: armorId == selectedArmor ? "blue" : "red" }}
+            >
               {armor}
             </button>
             <button onClick={() => delArmor(armorId)}>delete</button>
