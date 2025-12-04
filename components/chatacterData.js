471d1a43916281cec470f00601af1962cc105530
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-04 12:44:44 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -90,5 +90,5 @@ export const getSelected = (selectedItem = "selectedArmor", charId) => {
 export const selectItem = (item = "selectedArmor", characterId = 1, itemId) => {
   console.log("select item");
   console.log(characterId);
-  characterData[characterId][item] = itemId;
+  characterData[characterId - 1][item] = itemId;
 };
