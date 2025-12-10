84713a30a265ae04d4975de7a8a2cd559c18fb3e
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-09 19:32:47 -0800

    Update View.js

--- components/View.js
+++ components/View.js
@@ -63,6 +63,8 @@ export default View = ({ characterId }) => {
         setWeaponToAdd={setWeaponToAdd}
       />
       <DisplayWeapon
+        characterId={characterId}
+        delWeapon={viewDelteItem}
         viewDeleteWeapon={viewDeleteWeapon}
         characterWeapon={character.weapon}
       />
