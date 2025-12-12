7f97ff135a6bd25231fbaf89e4a4062f51277802
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-11 18:09:45 -0800

    Update AddWeapon.js

--- components/AddWeapon.js
+++ components/AddWeapon.js
@@ -2,15 +2,13 @@ import { useState } from "react";
 
 export default AddWeapon = ({ updateInventory, setWeaponToAdd }) => {
   availableWeapons = ["saber", "cutless"];
-  const [listWeapon, setListWeapon] = useState(availableWeapons[0]);
-
+  setWeaponToAdd(availableWeapons[0]);
   // will set state to first on weapon list
   const addNewWeapon = () => {
     updateInventory("weapon");
   };
   return (
     <div>
-      <p>Add {listWeapon}</p>
       <br />
       <select onChange={(e) => setWeaponToAdd(e.target.value)}>
         {availableWeapons.map((i) => (
