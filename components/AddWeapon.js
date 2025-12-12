8e67b174796f67fa639027d64c8808adfde1e4dc
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-12 15:50:38 -0800

    Update AddWeapon.js

--- components/AddWeapon.js
+++ components/AddWeapon.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import { addWeapon } from "./characterData.js";
 
 export default AddWeapon = ({ updateInventory, setWeaponToAdd }) => {
   availableWeapons = ["saber", "cutless"];
-  const [localWeapon, setLocalWeapon] = useState(availableWeapons[0])
+  const [localWeapon, setLocalWeapon] = useState(availableWeapons[0]);
   setWeaponToAdd(availableWeapons[0]);
   const addNewWeapon = () => {
-    updateInventory("weapon");
+    //updateInventory("weapon");
+    addWeapon(1, localWeapon);
   };
   return (
     <div>
       <br />
-      <select onChange={(e) => setWeaponToAdd(e.target.value)}>
+      <select onChange={(e) => setLocalWeapon(e.target.value)}>
         {availableWeapons.map((i) => (
           <option>{i}</option>
         ))}
