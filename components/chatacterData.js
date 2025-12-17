22e56504fb2da5e7e7fd0cc95584511aaa7b75c5
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-16 18:42:40 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -9,6 +9,7 @@ let characterData = [
     weapon: ["spear"],
     armor: [],
     selectedArmor: null,
+    selectedWeapon:null,
   },
   {
     id: 2,
@@ -20,6 +21,7 @@ let characterData = [
     weapon: ["sword"],
     armor: [],
     selectedArmor: null,
+    selectedWeapon:null,
   },
 ];
 
@@ -33,6 +35,62 @@ export const addCharacter = (newCharacter) => {
     let temp = [...characterData, newCharacter];
     characterData = temp;
   }
+};
+
+export const allCharacters = () => {
+  return characterData;
+};
+
+export const deleteCharacter = (deleteId) => {
+  let temp = characterData.filter((character) => character.id != deleteId);
+  characterData = temp;
+};
+
+export const singleCharacter = (id = 1) => {
+  let temp = characterData.find((character) => character.id == id);
+  return temp;
+};
+
+export const addWeapon = (awId = 0, newWeapon = "Seax") => {
+  console.log("add weapon");
+  console.log(awId);
+  console.log(newWeapon);
+  let temp = characterData[awId - 1].weapon;
+  temp.push(newWeapon);
+  characterData[awId].weapon = temp;
+};
+
+export const addInventory = (
+  characterId = 1,
+  type = "armor",
+  item = "scale"
+) => {
+  /*let temp = characterData[characterId - 1][type];
+  temp.push(item);
+  characterData[characterId][type] = temp;*/
+  let temp = characterData.find((character) => character.id == characterId);
+  temp = { ...temp, [type]: [...temp[type], item] };
+  characterData = characterData.map((character) =>
+    character.id == characterId ? temp : character
+  );
+};
+
+export const deleteItem = (delId = 0, charId = 1, reference = "armor") => {
+  console.log("delete Item");
+  //delete characterData[charId - 1][reference][delId];
+  console.log(delId, charId, reference);
+  characterData[charId - 1][reference].splice(delId, 1);
+  //console.log(characterData);
+};
+
+export const getSelected = (selectedItem = "selectedArmor", charId) => {
+  console.log("get selected");
+  return characterData[charId - 1]["selectedArmor"];
+};
+
+export const selectItem = (itemId, item = "selectedArmor", characterId = 1) => {
+  console.log("select item");
+  characterData[characterId - 1][item] = itemId;
 };
 
 export const allCharacters = () => {
