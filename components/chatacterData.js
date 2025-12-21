0c5d7905b389b3bab8895e51aec842080f41007c
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-21 14:04:30 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -29,7 +29,7 @@ let characterData = [
 export const multiItems = (itemPath = [1, "items", "bag"]) => {
   console.log("multi items");
 
-  let temp = itemPath.reduce((currentLevel, key) => {
+  itemPath.forEach((currentLevel, key) => {
     // Check if currentLevel is null, undefined, or the key doesn't exist
     console.log(
       currentLevel && typeof currentLevel === "object" && key in currentLevel
