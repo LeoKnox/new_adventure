b399d7b34d3b2d945b2b4ccf6c0a910a2af5f54a
LeoKnox <LeoKnox@users.noreply.github.com>
2025-12-25 16:30:00 -0800

    Update View.js

--- components/View.js
+++ components/View.js
@@ -64,7 +64,7 @@ export default View = ({ characterId }) => {
             <input
               type="checkbox"
               name="item"
-              value={current}
+              value={multiItems(current)}
               onChange={selectBag}
             />
           </label>
