42eef422a78ba45a72921f2ce432790cae3e2da9
LeoKnox <LeoKnox@users.noreply.github.com>
2026-01-07 16:22:37 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -37,7 +37,7 @@ export const multiItems = (itemPath = [1, "items", "bag"]) => {
   let y = itemPath.reduce((currentObj, key) => {
     // Check if the currentObj is valid and has the next key
     currentObj && currentObj[key] !== "undefined"
-      ? console.log("+" + "a")
+      ? console.log("+" + characterData[key])
       : console.log("+" + "b");
     //? currentObj[key]
     //: undefined;
