8faf043b76fc74dce86b04f9d5754fc2c8a471bf
LeoKnox <LeoKnox@users.noreply.github.com>
2026-01-08 16:15:19 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -43,7 +43,7 @@ export const multiItems = (itemPath = [1, "items", "bag"]) => {
     currentObj && currentObj[key] !== "undefined"
       ? //? console.log("+" + Object.keys(currentObj))
         //: console.log("+" + "b");
-        console.log("--" + characterData[key])
+        console.log("--" + characterData[currentObj])
       : undefined;
   }, characterData);
   /*let y = temp.forEach(
