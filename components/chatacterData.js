9653bdfe04c6f9019979e93196e36dbaae63adf2
LeoKnox <LeoKnox@users.noreply.github.com>
2026-01-10 18:35:51 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -28,9 +28,9 @@ let characterData = [
 
 export const multiItems = (itemPath = [1, "items", "bag"]) => {
   console.log("multi items");
-  let temp = itemPath;
-  console.log(temp);
-  let newtemp = temp.reduce((data, key) => {
+  let temp = [itemPath];
+  console.log(itemPath);
+  let newtemp = itemPath.reduce((data, key) => {
     return data !== undefined ? data[key] : undefined;
   }, characterData);
   console.log(newtemp);
