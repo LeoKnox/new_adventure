df390928969d01e44ab11794a7f23303394fd6c9
LeoKnox <LeoKnox@users.noreply.github.com>
2026-01-18 13:31:54 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -34,17 +34,17 @@ const testtest = (data) => {
 
 export const multiItems = (characterId = 1, itemPath = [1, "items", "bag"]) => {
   console.log("multi items");
-  let temp = ["items", "bags"];
+  let temp = [1, "items", "bags"];
   //let temp = [itemPath];
-  let newtempx = temp.reduce((data, key) => {
+  /*let newtempx = temp.reduce((data, key) => {
     return data && data[key] !== undefined
       ? console.log(JSON.stringify(data[key]) + ":" + key)
       : "undefined1";
-  }, characterData[characterId]);
+  }, characterData[characterId]);*/
   console.log("DDD");
   let newtemp = temp.reduce((data, key) => {
     return data?.[key];
-  }, characterData[characterId]);
+  }, characterData);
   console.log(newtemp);
   return newtemp;
 };
