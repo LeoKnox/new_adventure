cc09de55a79d674aade00ce51622c0dd80a21a9c
LeoKnox <LeoKnox@users.noreply.github.com>
2026-01-20 12:02:12 -0800

    Update View.js

--- components/View.js
+++ components/View.js
@@ -1,99 +1,124 @@
-import { useState, useEffect } from "react";
-import {
-  singleCharacter,
-  addInventory,
-  deleteItem,
-  selectItem,
-  retreiveWeapons,
-  multiItems,
-} from "./characterData.js";
-import AddArmor from "./AddArmor.js";
-import AddWeapon from "./AddWeapon.js";
-import DisplayArmor from "./DisplayArmor.js";
-import DisplayWeapon from "./DisplayWeapon.js";
+let characterData = [
+  {
+    id: 1,
+    name: "ao",
+    icon: 0,
+    lvl: 1,
+    atk: 10,
+    def: 10,
+    weapon: ["spear"],
+    armor: [],
+    selectedArmor: null,
+    selectedWeapon: null,
+  },
+  {
+    id: 2,
+    name: "aka",
+    icon: 0,
+    lvl: 1,
+    atk: 12,
+    def: 9,
+    weapon: ["sword"],
+    armor: [],
+    items: { bag: ["coffee"], pack: ["ocha"] },
+    selectedArmor: null,
+    selectedWeapon: null,
+  },
+];
 
-export default View = ({ characterId }) => {
-  const [character, setCharacter] = useState({});
-  const [currentBag, setCurrentBag] = useState(["items", "bag"]);
-  const [retreivedWeapons, setRetreivedWeapons] = useState(retreiveWeapons());
-  const [weaponToAdd, setWeaponToAdd] = useState("kama");
-  const updateInventory = (type = "armor", item = "leather") => {
-    addInventory(characterId, type, item);
+const testtest = (data) => {
+  console.log("test test");
+  console.log(data);
+  console.log(Object.keys(characterData[1][data]));
+};
 
-    setCharacter(singleCharacter(characterId));
-  };
-  console.log("view");
-  //console.log(multiItems([1, "items"]));
-  const selectBag = (e) => {
-    console.log("select bag");
-    console.log(e.target.checked);
-    console.log(e.target.value);
-    let temp = [...currentBag, e.target.value];
-    console.log(temp);
-    setCurrentBag(temp);
-    console.log(currentBag);
-  };
-  const viewDeleteItem = (viewId, itemDelete) => {
-    console.log("view delete weapon");
-    deleteItem(viewId, characterId, itemDelete);
-    setCharacter({ ...singleCharacter(characterId) });
-  };
-  const selectActiveArmor = (itemId, selectedItem = "selectedArmor") => {
-    console.log("select active armor");
-    selectItem(itemId, selectedItem, characterId);
-    setCharacter({ ...singleCharacter(characterId) });
-  };
-  useEffect(() => {
-    console.log("view use effect");
-    let temp = {};
-    temp = singleCharacter(characterId);
-    setCharacter(temp);
-  }, []);
+export const multiItems = (itemPath = [1, "items", "bag"], characterId = 1) => {
+  console.log("multi items");
+  //let temp = ["items", "bags"];
+  let y = [1, "items", "bags"];
+  let z = [...y];
+  console.log(itemPath);
+  let temp = [itemPath];
+  /*let newtempx = temp.reduce((data, key) => {
+    return key ? console.log(Object.keys(characterData[key])) : "undefined1";
+  }, characterData[characterId]);*/
+  console.log("DDD");
+  let newtemp = temp.reduce((data, key) => {
+    return data?.[key];
+  }, characterData[characterId]);
+  console.log(newtemp);
+  return newtemp;
+};
 
-  return (
-    <>
-      <h3>Character</h3>
-      <h3>id: {characterId}</h3>
-      <p>Name: {character.name}</p>
-      <p>Icon: {character.icon}</p>
-      <p>{character.weapon}</p>
-      <p>Lvl: {character.lvl}</p>
-      <p>Atk: {character.atk}</p>
-      <p>Def: {character.def}</p>
-      <button /*nClick={multiItems([2])}*/>items</button>
-      {currentBag}
-      {currentBag.map((current) => (
-        <p style={{ display: "inlineBlock" }}>
-          <label style={{ display: "flex", flexDirection: "column" }}>
-            <input
-              type="checkbox"
-              name="item"
-              value={current}
-              onChange={(e) => selectBag(e)}
-            />
-            {JSON.stringify(multiItems(current))}:{current}
-          </label>
-        </p>
-      ))}
+export const addCharacter = (newCharacter) => {
+  newCharacter.weapon = [];
+  newCharacter.armor = [];
+  if (characterData.length == 0) {
+    characterData = newCharacter;
+  } else {
+    newCharacter.id = characterData[characterData.length - 1].id + 1;
+    let temp = [...characterData, newCharacter];
+    characterData = temp;
+  }
+};
 
-      <AddWeapon
-        updateInventory={updateInventory}
-        setWeaponToAdd={setWeaponToAdd}
-        charId={characterId}
-      />
-      <DisplayWeapon
-        delWeapon={viewDeleteItem}
-        characterWeapon={retreivedWeapons}
-        charId={characterId}
-        selectActiveArmor={selectActiveArmor}
-      />
-      <AddArmor updateInventory={updateInventory} />
-      <DisplayArmor
-        selectActiveArmor={selectActiveArmor}
-        delArmor={viewDeleteItem}
-        characterArmor={character.armor}
-        charId={characterId}
-      />
-    </>
+export const allCharacters = () => {
+  return characterData;
+};
+
+export const deleteCharacter = (deleteId) => {
+  let temp = characterData.filter((character) => character.id != deleteId);
+  characterData = temp;
+};
+
+export const singleCharacter = (id = 1) => {
+  let temp = characterData.find((character) => character.id == id);
+  return temp;
+};
+
+export const addWeapon = (awId = 0, newWeapon = "Seax") => {
+  console.log("add weapon");
+  console.log(awId);
+  console.log(newWeapon);
+  let temp = characterData[awId - 1].weapon;
+  temp.push(newWeapon);
+  characterData[awId].weapon = temp;
+};
+
+export const addInventory = (
+  characterId = 1,
+  type = "armor",
+  item = "scale"
+) => {
+  /*let temp = characterData[characterId - 1][type];
+  temp.push(item);
+  characterData[characterId][type] = temp;*/
+  let temp = characterData.find((character) => character.id == characterId);
+  temp = { ...temp, [type]: [...temp[type], item] };
+  characterData = characterData.map((character) =>
+    character.id == characterId ? temp : character
   );
+};
+
+export const deleteItem = (delId = 0, charId = 1, reference = "armor") => {
+  console.log("delete Item");
+  //delete characterData[charId - 1][reference][delId];
+  console.log(delId, charId, reference);
+  characterData[charId - 1][reference].splice(delId, 1);
+  //console.log(characterData);
+};
+
+export const getSelected = (selectedItem = "selectedArmor", charId) => {
+  console.log("get selected");
+  return characterData[charId - 1][selectedItem];
+};
+
+export const selectItem = (itemId, item = "selectedArmor", characterId = 1) => {
+  console.log("select item");
+  characterData[characterId - 1][item] = itemId;
+};
+
+export const retreiveWeapons = (charId) => {
+  console.log("retreive weapons");
+  return characterData["1"]["weapon"];
 };
