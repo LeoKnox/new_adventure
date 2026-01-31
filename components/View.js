6bb757277956432c3cbaad27c0fccca5bcc10912
LeoKnox <LeoKnox@users.noreply.github.com>
2026-01-30 16:36:29 -0800

    Update View.js

--- components/View.js
+++ components/View.js
@@ -69,7 +69,7 @@ export default View = ({ characterId }) => {
           **{"" + Array.isArray(currentBag.slice(0, key + 1))}:
           {Array.isArray(selectBag(currentBag.slice(0, key + 1)))
             ? selectBag(currentBag.slice(0, key + 1))
-            : typeof selectBag(currentBag.slice(0, key + 1))}
+            : Object.keys(selectBag(currentBag.slice(0, key + 1)))}
         </ul>
       ))}
 
