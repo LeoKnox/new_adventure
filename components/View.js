38514eb53076302e5ff67e5eec0d48c9d0308b91
LeoKnox <LeoKnox@users.noreply.github.com>
2026-02-06 18:09:43 -0800

    Update View.js

--- components/View.js
+++ components/View.js
@@ -25,7 +25,7 @@ export default View = ({ characterId }) => {
   console.log("view");
   const selectBag = (temp, key) => {
     console.log("select bag");
-    console.log(typeof key === undefined);
+    console.log(JSON.stringify(key));
     let retTemp = [];
     retTemp = multiItems(temp, characterId);
     temp ?? setCurrentBag(temp);
