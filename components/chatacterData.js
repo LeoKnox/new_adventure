53580f43ff559be24212aff1b43a294bc08d8e8f
LeoKnox <LeoKnox@users.noreply.github.com>
2026-02-13 13:25:14 -0800

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -31,9 +31,99 @@ export const addToBag = (itemPath, characterId = 1) => {
 
   let temp = [characterId - 1, ...itemPath];
   console.log(temp);
-  let newtemp = temp.reduce(
-    (prev, curr) => prev?.[curr],
-    characterData[characterId]
+  let newtemp = temp.reduce((prev, curr) => prev?.[curr], characterData);
+  console.log(newtemp);
+  newtemp[0] = "coffee";
+
+  return newtemp;
+};
+
+export const multiItems = (itemPath, characterId = 1) => {
+  console.log("multi items");
+  let temp = [itemPath.unshift(characterId - 1)];
+  let newtemp = itemPath.reduce((prev, curr) => prev?.[curr], characterData);
+  return newtemp;
+};
+
+export const addMultiItems = (characterId = 1, keyBag = "key bag") => {
+  console.log("add multi items");
+  console.log(characterId);
+  console.log(keyBag);
+  characterData[characterId]["items"][keyBag] = Array(4).fill("*");
+};
+
+export const addCharacter = (newCharacter) => {
+  newCharacter.weapon = [];
+  newCharacter.armor = [];
+  if (characterData.length == 0) {
+    characterData = newCharacter;
+  } else {
+    newCharacter.id = characterData[characterData.length - 1].id + 1;
+    let temp = [...characterData, newCharacter];
+    characterData = temp;
+  }
+};
+
+export const allCharacters = () => {
+  return characterData;
+};
+
+export const deleteCharacter = (deleteId) => {
+  let temp = characterData.filter((character) => character.id != deleteId);
+  characterData = temp;
+};
+
+export const singleCharacter = (id = 1) => {
+  let temp = characterData.find((character) => character.id == id);
+  return temp;
+};
+
+export const addWeapon = (awId = 0, newWeapon = "Seax") => {
+  console.log("add weapon");
+  console.log(awId);
+  console.log(newWeapon);
+  let temp = characterData[awId - 1].weapon;
+  temp.push(newWeapon);
+  characterData[awId].weapon = temp;
+};
+
+export const addInventory = (
+  characterId = 1,
+  type = "armor",
+  item = "scale"
+) => {
+  /*let temp = characterData[characterId - 1][type];
+  temp.push(item);
+  characterData[characterId][type] = temp;*/
+  let temp = characterData.find((character) => character.id == characterId);
+  temp = { ...temp, [type]: [...temp[type], item] };
+  characterData = characterData.map((character) =>
+    character.id == characterId ? temp : character
+  );
+};
+
+export const deleteItem = (delId = 0, charId = 1, reference = "armor") => {
+  console.log("delete Item");
+  //delete characterData[charId - 1][reference][delId];
+  console.log(delId, charId, reference);
+  characterData[charId - 1][reference].splice(delId, 1);
+  //console.log(characterData);
+};
+
+export const getSelected = (selectedItem = "selectedArmor", charId) => {
+  console.log("get selected");
+  return characterData[charId - 1][selectedItem];
+};
+
+export const selectItem = (itemId, item = "selectedArmor", characterId = 1) => {
+  console.log("select item");
+  characterData[characterId - 1][item] = itemId;
+};
+
+export const retreiveWeapons = (charId) => {
+  console.log("retreive weapons");
+  return characterData["1"]["weapon"];
+};
   );
   console.log(newtemp);
   newtemp[0] = "coffee";
