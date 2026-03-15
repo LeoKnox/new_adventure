5e7d9c42e35972278ba07ee50c4ad0a63704ea73
LeoKnox <LeoKnox@users.noreply.github.com>
2026-03-15 14:29:16 -0700

    Update chatacterData.js

--- components/chatacterData.js
+++ components/chatacterData.js
@@ -149,7 +149,7 @@ export const deleteFromBag = (
   newItem = "newItem"
 ) => {
   const updateBag = (id, subKey, itemIndex, val) => {
-    return setCharacters.reduce((prev) =>
+    return deleteFromBag((prev) =>
       prev.map((c) =>
         c.id === id
           ? {
