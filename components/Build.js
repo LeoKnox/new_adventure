df06df8eaf2fe3561bf56ebcb8c3f46ebb5169d2
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-13 17:45:48 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -3,8 +3,15 @@ import { useState, useEffect } from "react";
 export default Build = () => {
   console.log("build.js");
   const [roomCoords, setRoomCoords] = useState([3.4]);
-  return <div style={{ width: '100%', height: `${roomCoords[1]}em`, backGroundColor:"red"}}>
-    red
-  
-</div>
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: `${roomCoords[1]*50}px`,
+        backgroundColor: "red",
+      }}
+    >
+      red
+    </div>
+  );
 };
