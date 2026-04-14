ea208554937918db57b2510ddb2031cf14916e81
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-13 17:54:09 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -7,7 +7,25 @@ export default Build = () => {
     <div
       style={{
         position: "absolute",
+        width: "100%",import { useState, useEffect } from "react";
+
+export default Build = () => {
+  console.log("build.js");
+  const [roomCoords, setRoomCoords] = useState([3,4]);
+  return (
+    <div
+      style={{
+        position: "absolute",
         width: "100%",
+        height: `${roomCoords[1]*40}px`,
+        backgroundColor: "red",
+      }}
+    >
+      red{roomCoords[1]}
+    </div>
+  );
+};
+
         height: `${roomCoords[1]}px`,
         backgroundColor: "red",
       }}
