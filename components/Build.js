3fb680e71265370a701733668c4482d8068c21b0
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-13 17:56:10 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -2,35 +2,16 @@ import { useState, useEffect } from "react";
 
 export default Build = () => {
   console.log("build.js");
-  const [roomCoords, setRoomCoords] = useState([3.4]);
+  const [tileSize, setTilesize] = useState(40);
+  const [roomCoords, setRoomCoords] = useState([3, 4]);
   return (
     <div
       style={{
         position: "absolute",
-        width: "100%",import { useState, useEffect } from "react";
-
-export default Build = () => {
-  console.log("build.js");
-  const [roomCoords, setRoomCoords] = useState([3,4]);
-  return (
-    <div
-      style={{
-        position: "absolute",
-        width: "100%",
-        height: `${roomCoords[1]*40}px`,
+        width: `${roomCoords[0] * tileSize}px`,
+        height: `${roomCoords[1] * tileSize}px`,
         backgroundColor: "red",
       }}
-    >
-      red{roomCoords[1]}
-    </div>
-  );
-};
-
-        height: `${roomCoords[1]}px`,
-        backgroundColor: "red",
-      }}
-    >
-      red{roomCoords[1]}
-    </div>
+    ></div>
   );
 };
