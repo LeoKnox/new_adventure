84ad25db59db9517ed0eda0c18f44525d74362fd
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-14 18:02:59 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
+//import myImage from "./Lava_Background.webp";
 
 export default Build = () => {
   console.log("build.js");
   const [tileSize, setTilesize] = useState(40);
-  const [roomCoords, setRoomCoords] = useState([3, 4]);
+  const [roomCoords, setRoomCoords] = useState([3, 4, 10, 8]);
   return (
     <div
       style={{
         position: "absolute",
         width: `${roomCoords[0] * tileSize}px`,
         height: `${roomCoords[1] * tileSize}px`,
+        left: `${roomCoords[2]*tileSize}px`,
+        top: `${roomCoords[3]*tileSize}px`,
+
         backgroundColor: "red",
       }}
     ></div>
