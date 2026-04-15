b71c64462fe36c68a82ba10c64266978b88eca26
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-14 18:55:35 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -3,16 +3,17 @@ import { useState, useEffect } from "react";
 
 export default Build = () => {
   console.log("build.js");
+  const pagePosition = [40, 160];
   const [tileSize, setTilesize] = useState(40);
-  const [roomCoords, setRoomCoords] = useState([3, 4, 10, 8]);
+  const [roomCoords, setRoomCoords] = useState([3, 4]);
   return (
     <div
       style={{
         position: "absolute",
         width: `${roomCoords[0] * tileSize}px`,
         height: `${roomCoords[1] * tileSize}px`,
-        left: `${roomCoords[2]*tileSize}px`,
-        top: `${roomCoords[3]*tileSize}px`,
+        left: `${pagePosition[0]}px`,
+        top: `${pagePosition[1]}px`,
 
         backgroundColor: "red",
       }}
