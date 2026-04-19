da86d0662b4c37383a583fbaf19031d263568564
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-19 15:16:33 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -9,52 +9,55 @@ export default Build = () => {
   const [doors, setDoors] = useState([3, 2]);
   return (
     <>
-      <div style={{ display: "flex" }}>
-        <input
-          onChange={() => setTilesize(40)}
-          type="radio"
-          id="40"
-          name="tileSize"
-          value="40"
-          checked={tileSize == 40}
-        />
-        <label>40</label>
-        <input
-          onChange={() => setTilesize(30)}
-          type="radio"
-          id="30"
-          name="tileSize"
-          value="30"
-          checked={tileSize == 30}
-        />
-        <label>30</label>
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          border: `${tileSize}px solid black`,
-          width: `${room.roomCoords[0] * tileSize}px`,
-          height: `${room.roomCoords[1] * tileSize}px`,
-          left: `${200 - pagePosition[0]}px`,
-          top: `${pagePosition[1]}px`,
+      
+        <div style={{ display: "flex" }}>
+          <input
+            onChange={() => setTilesize(40)}
+            type="radio"
+            id="40"
+            name="tileSize"
+            value="40"
+            checked={tileSize == 40}
+          />
+          <label>40</label>
+          <input
+            onChange={() => setTilesize(30)}
+            type="radio"
+            id="30"
+            name="tileSize"
+            value="30"
+            checked={tileSize == 30}
+          />
+          <label>30</label>
+        </div>
+        <div style={{ width: "400px", backgroundColor: "gray" }}>
+        <div
+          style={{
+            position: "absolute",
+            border: `${tileSize}px solid black`,
+            width: `${room.roomCoords[0] * tileSize}px`,
+            height: `${room.roomCoords[1] * tileSize}px`,
+            left: `${200 - pagePosition[0]}px`,
+            top: `${pagePosition[1]}px`,
 
-          backgroundColor: "red",
-        }}
-      >
-        {doors[1]}:{roomCoords[1]}:{pagePosition[1]}
+            backgroundColor: "red",
+          }}
+        >
+          {doors[1]}:{roomCoords[1]}:{pagePosition[1]}
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            width: `${tileSize}px`,
+            height: `${tileSize}px`,
+            left: `${200 - pagePosition[0] + room.doors[0] * tileSize}px`,
+            top: `${
+              pagePosition[1] + room.roomCoords[1] * tileSize + tileSize
+            }px`,
+            backgroundColor: "brown",
+          }}
+        ></div>
       </div>
-      <div
-        style={{
-          position: "absolute",
-          width: `${tileSize}px`,
-          height: `${tileSize}px`,
-          left: `${200 - pagePosition[0] + room.doors[0] * tileSize}px`,
-          top: `${
-            pagePosition[1] + room.roomCoords[1] * tileSize + tileSize
-          }px`,
-          backgroundColor: "brown",
-        }}
-      ></div>
     </>
   );
 };
