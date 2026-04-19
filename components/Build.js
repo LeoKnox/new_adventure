33f4096e618ebf831264615f78d67d5f751bf34e
LeoKnox <LeoKnox@users.noreply.github.com>
2026-04-19 12:20:59 -0700

    Update Build.js

--- components/Build.js
+++ components/Build.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 
 export default Build = () => {
   console.log("build.js");
-  const pagePosition = [40, 160];
-  const [room, setRoom] = useState({ roomCoords: [2, 3], doors: [2, 5] });
   const [tileSize, setTilesize] = useState(40);
+  const pagePosition = [{ tileSize }, 160];
+  const [room, setRoom] = useState({ roomCoords: [2, 3], doors: [2, 5] });
+
   const [roomCoords, setRoomCoords] = useState([3, 4]);
   const [doors, setDoors] = useState([3, 2]);
   return (
